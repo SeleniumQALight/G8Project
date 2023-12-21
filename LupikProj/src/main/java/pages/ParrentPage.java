708850a6e1695cc5489a3303b4f8b999ad206405
Aleksandr Lupik <sashaloopik@gmail.com>
2023-12-21 21:50:31 +0200

    first test

---
+++ LupikProj/src/main/java/pages/ParrentPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+//все загальне для сторінок
+public class ParrentPage extends CommonActionsWithElements{
+
+    public ParrentPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+}
