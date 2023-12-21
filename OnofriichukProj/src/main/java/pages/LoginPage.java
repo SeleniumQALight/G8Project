8d02042691ef44dbf453ff6374ce7c03c1649e35
IrynaOnofriichuk <irina1989ovcharenko@gmail.com>
2023-12-21 21:49:52 +0200

    Class work commit

---
+++ OnofriichukProj/src/main/java/pages/LoginPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends ParentPage {
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void openLoginPage() {
+        try {
+            webDriver.get("https://aqa-complexapp.onrender.com");
+            logger.info("Login page was opened");
+        } catch (Exception e) {
+            logger.error("Can not open login page");
+            Assert.fail("Can not open login page");
+        }
+    }
+
+    public void enterTextIntoInputLogin(String login) {
+        WebElement inputLogin =
+                webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
+        enterTextIntoInput(inputLogin, login);
+
+
+    }
+    public void enterTextIntoInputPassword(String password) {
+        WebElement inputPassword =
+                webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+        enterTextIntoInput(inputPassword, password);
+    }
+
+    public void clickOnButtonSingIn() {
+        WebElement buttonSingIn =
+                webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
+        clickOnElement(buttonSingIn);
+    }
+}
