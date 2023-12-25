20ae5c6a8354904bdd520d7b45b2782638350743
tymofiiyashchenko <tymofii.yashchenko@airslate.com>
2023-12-25 19:58:35 +0200

    HW3 Invalid login autotest with Page Object

--- YashchenkoProj/src/main/java/pages/HomePage.java
+++ YashchenkoProj/src/main/java/pages/HomePage.java
@@ -10,9 +10,13 @@ public class HomePage extends ParentPage {
     }
 
     public boolean isButtonSignOutVisible() {
-        WebElement buttonSignOut
-                = webDriver.findElement(By.xpath("//button[text()='Sign Out']"));
-        return isElementDisplayed(buttonSignOut);
+        try {
+            WebElement buttonSignOut
+                    = webDriver.findElement(By.xpath("//button[text()='Sign Out']"));
+            return isElementDisplayed(buttonSignOut);
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
--- YashchenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ YashchenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -12,8 +12,21 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
         pageProvider.getLoginPage().clickOnButtonSignIn();
 
-        Assert.assertTrue("Button SignOut is not visible",
+        Assert.assertTrue("Button SignOut is visible",
                 pageProvider.homePage().isButtonSignOutVisible());
+
+    }
+
+    @Test
+    public void invalidLogin() {
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin("qaavto");
+        pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+
+        Assert.assertFalse("Button SignOut is not visible", pageProvider.homePage().isButtonSignOutVisible());
+        Assert.assertTrue("Button SignIn is visible", pageProvider.getLoginPage().isButtonSignInVisible());
+        Assert.assertTrue("Alert is visible", pageProvider.getLoginPage().isInvalidUserNamePasswordAlertVisible());
 
     }
 }
