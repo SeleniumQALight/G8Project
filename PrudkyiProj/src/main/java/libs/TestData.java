54cefa1a47f0ae75d703203b5c950df254cb6783
Hryhorii77 <serhii.diachenko75@gmail.com>
2023-12-26 20:55:47 +0200

    test data class was added

---
+++ PrudkyiProj/src/main/java/libs/TestData.java
@@ -0,0 +1,8 @@
+package libs;
+
+//клас для зберігання тестових даних
+//які ми будемо використовувати в тестах багато разів
+public class TestData {
+    public final static String VALID_LOGIN_UI = "qaauto";
+    public final static String VALID_PASSWORD_UI = "123456qwerty";
+}
--- PrudkyiProj/src/test/java/loginTests/LoginTextWithPageObject.java
+++ PrudkyiProj/src/test/java/loginTests/LoginTextWithPageObject.java
@@ -1,15 +1,19 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import libs.TestData;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static libs.TestData.VALID_LOGIN_UI;
+import static libs.TestData.VALID_PASSWORD_UI;
+
 public class LoginTextWithPageObject extends BaseTest {
     @Test
-    public void validLogin(){
+    public void validLogin() {
         pageProvider.loginPage().openLoginPage();
-        pageProvider.loginPage().enterTextIntoInputLogin("qaauto");
-        pageProvider.loginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.loginPage().enterTextIntoInputLogin(VALID_LOGIN_UI);
+        pageProvider.loginPage().enterTextIntoInputPassword(VALID_PASSWORD_UI);
         pageProvider.loginPage().clickOnButtonSignIn();
         Assert.assertTrue("Button Sing Out is not visible", pageProvider.homePage().isButtonSingOutVisible());
     }
