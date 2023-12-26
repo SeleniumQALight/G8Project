1952e3c889bd0028533c51517dd1e0eb30f14370
NickSemerenko <nickolay.sem@gmail.com>
2023-12-26 20:55:26 +0200

    TestData added

---
+++ SemerenkoProj/src/main/java/libs/TestData.java
@@ -0,0 +1,8 @@
+package libs;
+
+//клас ля зберігання тестових даних
+//які будуть використовуватися в тестах багато разів
+public class TestData {
+    public final static String VALID_LOGIN_UI = "qaauto";
+    public final static String VALID_PASSWORD_UI = "123456qwerty";
+}
--- SemerenkoProj/src/test/java/LoginTests/LoginTestWithPageObject.java
+++ SemerenkoProj/src/test/java/LoginTests/LoginTestWithPageObject.java
@@ -1,15 +1,19 @@
 package LoginTests;
 
 import baseTest.BaseTest;
+import libs.TestData;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static libs.TestData.VALID_LOGIN_UI;
+import static libs.TestData.VALID_PASSWORD_UI;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
     public void validLogin() {
         pageProvider.loginPage().openLoginPage();
-        pageProvider.loginPage().enterTextIntoInputLogin("qaauto");
-        pageProvider.loginPage().enterTextIntoInputPass("123456qwerty");
+        pageProvider.loginPage().enterTextIntoInputLogin(VALID_LOGIN_UI);
+        pageProvider.loginPage().enterTextIntoInputPass(VALID_PASSWORD_UI);
         pageProvider.loginPage().clickOnButtonSignIn();
 
         Assert.assertTrue("Button SignOut is not displayed",
