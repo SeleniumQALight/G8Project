83b8676b88aa7f4b3940c5d1ac29f6eb22ff212c
tanyaBulakh <bulakhtanya94@gmail.com>
2023-12-26 20:17:05 +0200

    elements with FindBy

--- BulakhProj/src/main/java/pages/LoginPage.java
+++ BulakhProj/src/main/java/pages/LoginPage.java
@@ -2,11 +2,20 @@ package pages;
 
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage  extends ParentPage{
+
+    @FindBy(xpath = "//button[contains(text(),'Sign In')]")
+    private WebElement buttonSingIn; // цей елемент створиться PageFactory в батьківському класі CommonActionsWithElements
+
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    private WebElement inputLogin;
+
+    @FindBy(xpath = "//input[@placeholder='Password']")
+    private WebElement inputPassword;
 
 
     public LoginPage(WebDriver webDriver) {
@@ -24,17 +33,26 @@ public class LoginPage  extends ParentPage{
     }
 
     public void enterTextInToInputLogin(String login) {
-        WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
+//        WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
         enterTextInToInput(inputLogin, login);
     }
 
     public void enterTextInToInputPassword(String password) {
-        WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+//        WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
         enterTextInToInput(inputPassword, password);
     }
 
     public void clickOnButtonSingIn() {
-        WebElement buttonSingIn = webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
+//        WebElement buttonSingIn = webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
         clickOnElement(buttonSingIn);
     }
+
+    // is button Sing In visible
+    public boolean isButtonSingInVisible() {
+//        WebElement buttonSingIn =
+//                webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
+        return isElementDisplayed(buttonSingIn);
+    }
+
+
 }
