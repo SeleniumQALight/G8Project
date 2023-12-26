0957b1b680e601f5ed3ac877dbe8ac617ffec0f6
Myrhorodska <schatznata87@gmail.com>
2023-12-26 19:55:10 +0100

    TestData.java was added

--- MyrhorodskaProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ MyrhorodskaProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -1,15 +1,19 @@
 package loginTest;
 
 import BaseTest.BaseTest;
+import libs.TestData;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static libs.TestData.VALID_LOGIN_UI;
+import static libs.TestData.VALID_PASSWORD_UI;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
     public void validLogin() {
         pageProvider.loginPage().openLoginPage();
-        pageProvider.loginPage().enterTextInToInputLogin("qaauto");
-        pageProvider.loginPage().enterTextInToInputPassword("123456qwerty");
+        pageProvider.loginPage().enterTextInToInputLogin(VALID_LOGIN_UI);
+        pageProvider.loginPage().enterTextInToInputPassword(VALID_PASSWORD_UI);
         pageProvider.loginPage().clickOnButtonSingIn();
 
         Assert.assertTrue("Button sign out is not visible",
