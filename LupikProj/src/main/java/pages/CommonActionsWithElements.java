bb72675aef48b863b7f107b40d6b622a2508c317
Aleksandr Lupik <sashaloopik@gmail.com>
2023-12-26 20:45:42 +0200

    getElementName

--- LupikProj/src/main/java/pages/CommonActionsWithElements.java
+++ LupikProj/src/main/java/pages/CommonActionsWithElements.java
@@ -8,35 +8,46 @@ import org.openqa.selenium.support.PageFactory;
 
 public class CommonActionsWithElements {
     protected WebDriver webDriver;
-   protected Logger logger = Logger.getLogger(getClass());
+    protected Logger logger = Logger.getLogger(getClass());
 
     public CommonActionsWithElements(WebDriver webDriver) {
 
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this); //для ініціалізації елементів сторінки опираючись на анотації @FindBy
     }
+
     protected void enterTextInToInput(WebElement element, String text) {
         try {
             element.clear();
             element.sendKeys(text);
-            logger.info(text + " was inputted into input");
+            logger.info(text + " was inputted into input " + getElementName(element));
         } catch (Exception e) {
-            logger.error("Can not work with element");
-            Assert.fail("Can not work with element");
+            logger.error("Can not work with element" + getElementName(element));
+            Assert.fail("Can not work with element" + getElementName(element));
         }
     }
+
+    protected String getElementName(WebElement webElement) {
+        try {
+            return webElement.getAccessibleName();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
 
     protected void clickOnElement(WebElement element) {
         try {
+            String elementName = getElementName(element);
             element.click();
-            logger.info("Element was clicked");
+            logger.info("Element was clicked " + elementName);
         } catch (Exception e) {
             logger.error("Can not work with element");
             Assert.fail("Can not work with element");
         }
     }
 
-protected boolean isElementDisplayed(WebElement element) {
+    protected boolean isElementDisplayed(WebElement element) {
         try {
             boolean state = element.isDisplayed();
             logger.info("Element is displayed -> " + state);
