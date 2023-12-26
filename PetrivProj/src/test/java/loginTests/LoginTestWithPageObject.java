24d3ef799abc8281961e3c515afe1bea8ea3539e
Yuliia Petriv <yuli.petriv@gmail.com>
2023-12-26 20:55:19 +0200

    TestData.java was added

--- PetrivProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ PetrivProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -4,12 +4,15 @@ import baseTest.BaseTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static libs.TestData.VALID_LOGIN_UI;
+import static libs.TestData.VALID_PASSWORD_UI;
+
 public class LoginTestWithPageObject extends BaseTest {
     @Test
     public void validLogin() {
         pageProvider.loginPage().openLoginPage();
-        pageProvider.loginPage().enterTextIntoInputLogin("qaauto");
-        pageProvider.loginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.loginPage().enterTextIntoInputLogin(VALID_LOGIN_UI);
+        pageProvider.loginPage().enterTextIntoInputPassword(VALID_PASSWORD_UI);
         pageProvider.loginPage().clickOnButtonSignIn();
         Assert.assertTrue("Button Sign Out is not visible", pageProvider.homePage().isButtonSignOutVisible());
     }
@@ -18,7 +21,7 @@ public class LoginTestWithPageObject extends BaseTest {
     public void inValidLogin() {
         pageProvider.loginPage().openLoginPage();
         pageProvider.loginPage().enterTextIntoInputLogin("invalid_username");
-        pageProvider.loginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.loginPage().enterTextIntoInputPassword(VALID_PASSWORD_UI);
         pageProvider.loginPage().clickOnButtonSignIn();
         Assert.assertTrue("Button Sign In is not visible", pageProvider.homePage().isButtonSignInVisible());
         Assert.assertTrue("Error message is not visible", pageProvider.homePage().isInvalidUsernameOrPasswordMessageVisible());
