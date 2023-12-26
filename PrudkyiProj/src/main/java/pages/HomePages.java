9100011fc3c26f4e64a5cd9bdb21318c24360eb2
Hryhorii77 <serhii.diachenko75@gmail.com>
2023-12-26 20:17:30 +0200

    added elements with FindBy

--- PrudkyiProj/src/main/java/pages/HomePages.java
+++ PrudkyiProj/src/main/java/pages/HomePages.java
@@ -1,16 +1,19 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePages extends ParentPage {
+    @FindBy(xpath = "//button[text()='Sign Out']")
+    private WebElement buttonSingOut;
+
     public HomePages(WebDriver webDriver) {
         super(webDriver);
     }
 
     public boolean isButtonSingOutVisible() {
-        WebElement buttonSingOut = webDriver.findElement(By.xpath("//button[text()='Sign Out']"));
+       // WebElement buttonSingOut = webDriver.findElement(By.xpath("//button[text()='Sign Out']"));
         return isElementDisplayed(buttonSingOut);
     }
 }
--- PrudkyiProj/src/main/java/pages/LoginPage.java
+++ PrudkyiProj/src/main/java/pages/LoginPage.java
@@ -1,11 +1,18 @@
 package pages;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends ParentPage {
+    @FindBy(xpath = ".//button[contains(text(),'Sign In')]")
+    private WebElement buttonSignIn;// цей елемент створиться тільки тоді коли ми використаємо анотацію @FindBy
+    @FindBy(xpath = ".//input[@placeholder='Username']")
+    private WebElement inputLogin;
+    @FindBy(xpath = ".//input[@placeholder='Password']")
+    private WebElement inputPassword;
+
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -21,18 +28,27 @@ public class LoginPage extends ParentPage {
     }
 
     public void enterTextIntoInputLogin(String login) {
-        WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
+        //WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
         enterTextIntoInputLogin(inputLogin, login);
     }
 
     public void enterTextIntoInputPassword(String password) {
-        WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+        //WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
         enterTextIntoInputLogin(inputPassword, password);
     }
 
     public void clickOnButtonSignIn() {
-        WebElement buttonSignIn = webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
+        //  WebElement buttonSignIn = webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
         clickOnElement(buttonSignIn);
 
     }
+
+    //is button SingnIn visible
+    public boolean isButtonSingInVisible() {
+        // WebElement buttonSingIn = webDriver.findElement(By.xpath(".//button[contains(text(),'Sign In')]"));
+        return isElementDisplayed(buttonSignIn);
+
+
+    }
 }
+
