c7a9c02094e2f57f3a8d2a8bdaeea45aa5cd56a2
Olesia Storozhuk <fneerkl@gmail.com>
2023-12-26 18:15:26 +0200

    Storozhuk-19-12

---
+++ StorozhukProj/src/main/java/pages/ParentPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+//everything that is common for all pages
+public class ParentPage extends CommonActionsWithElements {
+
+    public ParentPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+}
