8b26e99782113d938bc729d6e6588da5650996df
IUAD0CVU <andrey.tepliuk@raiffeisen.ua>
2023-12-27 21:29:56 +0200

    Classwork_Tepliuk-21-12

---
+++ TepliukProj/src/main/java/pages/PageProvider.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageProvider {
+    private WebDriver webDriver;
+    public PageProvider(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public LoginPage loginPage(){
+        return new LoginPage(webDriver);
+    }
+
+    public HomePage homePage() {
+    return new HomePage(webDriver);
+    }
+}
