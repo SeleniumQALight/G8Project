fa7a0cd9cc33af576a5943c58771d0bac05bc980
tradulenko <radulenko@gmail.com>
2023-12-28 21:43:14 +0200

    CreateNewPostTest.java part2

--- overAll/src/main/java/pages/HomePage.java
+++ overAll/src/main/java/pages/HomePage.java
@@ -2,25 +2,24 @@ package pages;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import pages.elements.HeaderElement;
 
 public class HomePage extends ParentPage {
-    @FindBy(xpath = ".//button[text()='Sign Out']")
-    private WebElement buttonSignOut;
+    private HeaderElement headerElement;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
-    public boolean isButtonSignOutVisible() {
-        return isElementDisplayed(buttonSignOut);
-    }
 
     public HomePage checkIsRedirectToHomePage() {
         // TODO check url
         Assert.assertTrue("Invalid page - not Home Page"
-                , isButtonSignOutVisible());
+                , getHeader().isButtonSignOutVisible());
         return this;
+    }
+
+    public HeaderElement getHeader() {
+        return new HeaderElement(webDriver);
     }
 }
--- overAll/src/test/java/postsTests/CreateNewPostTest.java
+++ overAll/src/test/java/postsTests/CreateNewPostTest.java
@@ -1,16 +1,24 @@
 package postsTests;
 
 import baseTest.BaseTest;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
     @Test
-    @Ignore
     public void createNewPost() {
         pageProvider.loginPage()
                 .openLoginPageAndFillLoginFormWithValidCred()
                 .checkIsRedirectToHomePage()
+                .getHeader().clickOnButtonCreatePost()
+                .checkIsRedirectToCreatePostPage()
+                .enterTitleInToInputTitle("radulenko title")
+                .enterTextIntoInputBody("body text")
+                //.selectTextInDropDown("Приватне повідомлення")
+                .selectValueInDropDown("One Person")
+                .clickOnSaveNewPostButton()
+                .checkIsRedirectToPostPage()
+                .checkIsSuccessMessageDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
         ;
     }
 }
