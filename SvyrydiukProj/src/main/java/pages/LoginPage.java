0377e3676da764259fcbe9a2988486045ca23238
romio1371 <romio1371@gmail.com>
2023-12-30 01:18:47 +0200

    HW4_Svyrydiuk_G8 Add Additional Check For Valid login test and Add check validation message for Sign Up form

--- SvyrydiukProj/src/main/java/pages/LoginPage.java
+++ SvyrydiukProj/src/main/java/pages/LoginPage.java
@@ -8,14 +8,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends ParentPage {
-@FindBy(xpath = ".//button[text()='Sign In']") // this element will crreated in PageFactory in CommonActionsWithElements.java
+    @FindBy(xpath = ".//button[text()='Sign In']")
+    // this element will crreated in PageFactory in CommonActionsWithElements.java
     private WebElement buttonSignIn;
 
-@FindBy(xpath = ".//input[@placeholder='Username']")
+    @FindBy(xpath = ".//input[@placeholder='Username']")
     private WebElement inputLogin;
 
-@FindBy(xpath = ".//input[@placeholder='Password']")
+    @FindBy(xpath = ".//input[@placeholder='Password']")
     private WebElement inputPassword;
+    @FindBy(xpath = ".//input[@id='username-register']")
+    private WebElement inputUsernameRegistration;
+
+    @FindBy(xpath = ".//input[@id='username-register']/..//div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']")
+    private WebElement validationMessageForUserNameRegistrationField;
+    @FindBy(xpath = ".//input[@id='password-register']")
+    private WebElement inputPasswordRegistration;
+    @FindBy(xpath = ".//input[@id='email-register']")
+    private WebElement inputEmailRegistration;
+    @FindBy(xpath = ".//input[@id='email-register']/following-sibling::div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']")
+    private WebElement validationMessageForEmailRegistrationField;
+    @FindBy(xpath = ".//input[@id='password-register']/following-sibling::div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']")
+    private WebElement validationMessageForPasswordRegistrationField;
+    @FindBy(xpath = ".//button[text()='Sign up for OurApp']")
+    private WebElement buttonSignUp;
 
 
     public LoginPage(WebDriver webDriver) {
@@ -33,7 +49,7 @@ public class LoginPage extends ParentPage {
     }
 
     public void enterTextIntoInputLogin(String login) {
-       // WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
+        // WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
         enterTextIntoInput(inputLogin, login);
     }
 
@@ -58,11 +74,11 @@ public class LoginPage extends ParentPage {
 
     public boolean isButtonSignInVisible() {
         //try {
-            //WebElement buttonSignIn = webDriver.findElement(By.xpath(".//button[text()='Sign In']"));
-            return isElementDisplayed(buttonSignIn);
+        //WebElement buttonSignIn = webDriver.findElement(By.xpath(".//button[text()='Sign In']"));
+        return isElementDisplayed(buttonSignIn);
         //} catch (Exception e) {
         //    return false;
-       // }
+        // }
     }
     //is button Sign In visible
     public HomePage openLoginPageAndFillLoginFormWithValidCred() {
@@ -73,4 +89,56 @@ public class LoginPage extends ParentPage {
         return new HomePage(webDriver);
     }
 
+    //is username field visible
+
+    public boolean isUsernameFieldVisible() {
+        return isElementDisplayed(inputLogin);
+    }
+
+    //is password field visible
+
+    public boolean isPasswordFieldVisible() {
+        return isElementDisplayed(inputPassword);
+    }
+
+
+    //enter text into input username registration
+    public void enterTextIntoInputUsernameRegistration(String username) {
+        enterTextIntoInput(inputUsernameRegistration, username);
+    }
+
+    public boolean isValidationMessageForUserNameFieldVisible() {
+        return isElementDisplayed(validationMessageForUserNameRegistrationField);
+    }
+
+    public Object checkTextValidationMessageForUserNameRegistrationField(String message) {
+        checkTextInElement(validationMessageForUserNameRegistrationField, message);
+        return this;
+    }
+
+    public void enterTextIntoInputPasswordRegistration(String password) {
+        enterTextIntoInput(inputPasswordRegistration, password);
+    }
+
+    public boolean isValidationMessageForPasswordFieldVisible() {
+        return isElementDisplayed(validationMessageForPasswordRegistrationField);
+    }
+    public void checkTextValidationMessageForPasswordRegistrationField(String message) {
+        checkTextInElement(validationMessageForPasswordRegistrationField, message);
+    }
+
+    public boolean isValidationMessageForEmailFieldVisible() {
+        return isElementDisplayed(validationMessageForEmailRegistrationField);
+    }
+    public void enterTextIntoInputEmailRegistration(String email) {
+        enterTextIntoInput(inputEmailRegistration, email);
+    }
+
+    public void checkTextValidationMessageForEmailRegistrationField(String message) {
+        checkTextInElement(validationMessageForEmailRegistrationField, message);
+    }
+
+    public void clickOnButtonSignUp() {
+        clickOnElement(buttonSignUp);
+    }
 }
