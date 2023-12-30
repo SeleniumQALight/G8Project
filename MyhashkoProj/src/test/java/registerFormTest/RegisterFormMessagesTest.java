5a892ade55a4c8806f17bc3b3378836ac1dfff9d
HruhoriiMyhashko <ssadhu1992@gmail.com>
2023-12-30 17:34:01 +0200

    HW_4_part_3 Чомусь отримую не зелений тест коли є перевірка наявності тексту "Username must be at least 3 characters." прошу підказати що не так роблю, бо локатор правильний і 2 інші перевірки ок ящко без цієї.

---
+++ MyhashkoProj/src/test/java/registerFormTest/RegisterFormMessagesTest.java
@@ -0,0 +1,22 @@
+package registerFormTest;
+
+import baseTest.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegisterFormMessagesTest extends BaseTest {
+    @Test
+    public void RegisterFormMessagesTest() {
+        pageProvider.loginPage().openLoginPage();
+        pageProvider.loginPage().enterTextIntoInputUsernameRegister("tr");
+        pageProvider.loginPage().enterTextIntoInputEmailRegister("tr");
+        pageProvider.loginPage().enterTextIntoInputPasswordRegister("tr");
+        pageProvider.loginPage().clickOnButtonSignUpRegister();
+
+
+        Assert.assertTrue("Error message is visible for username", pageProvider.loginPage().isErrorMessageVisibleForUsernameInput());
+        Assert.assertTrue("Error message is visible for email", pageProvider.loginPage().isErrorMessageVisibleForEmailInput());
+        Assert.assertTrue("Error message is visible for password", pageProvider.loginPage().isErrorMessageVisibleForPasswordInput());
+
+    }
+}
