1882953fe7f984eae5d4e64642c78504fa814cf8
Olesia Storozhuk <fneerkl@gmail.com>
2024-01-02 21:24:46 +0200

    HW_2_InvalidLoginTest

---
+++ StorozhukProj/src/test/java/loginTests/InvalidLoginTest.java
@@ -0,0 +1,102 @@
+package loginTests;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class InvalidLoginTest {
+    WebDriver webDriver;
+    Logger logger = Logger.getLogger(getClass());
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();     //.m2
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        logger.info("Browser was opened");
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+        logger.info("Browser was closed");
+    }
+
+    @Test
+    public void InvalidLogin() {
+        webDriver.get("https://aqa-complexapp.onrender.com/");
+        logger.info("Site was opened");
+
+        WebElement inputUserName = webDriver.findElement(By.xpath(".//input[@placeholder='Username']"));
+        inputUserName.clear();
+        inputUserName.sendKeys("qto1234");
+        logger.info("'qto1234' was inputted into input UserName");
+
+        WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("123456qwerty");
+        logger.info("password was inputted");
+
+        webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).
+
+                click();
+        logger.info("Button Sign In was clicked");
+
+        Assert.assertTrue("Button sign out isn't visible",
+
+                isButtonSignInVisible());
+        Assert.assertTrue("Button sign out isn't visible",
+
+                isButtonSignOutUnavailable());
+        Assert.assertTrue("Text is displayed",
+
+                isWrongUserNameOrPasswordMessageDisplayed());
+    }
+
+    private boolean isButtonSignInVisible() {
+        try {
+            boolean state = webDriver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).isDisplayed();
+            logger.info(state + " is button visible");
+            return state;
+        } catch (Exception e) {
+            logger.info("Element is not displayed");
+            return false;
+        }
+    }
+
+    public boolean isButtonSignOutUnavailable() {
+        try {
+            boolean state = webDriver.findElement(By.xpath("//button[contains(text(),'Sign Out')]")).isEnabled();
+            logger.info(!state + " is button unavailable");
+            return false;
+        } catch (Exception e) {
+            logger.info("Sign Out is hidden");
+            return true;
+        }
+    }
+
+    public boolean isWrongUserNameOrPasswordMessageDisplayed() {
+        try {
+            boolean state = webDriver.findElement(By.xpath("//*[contains (text(),'Invalid username/password.')]")).isEnabled();
+            logger.info("Message 'Invalid username/password' is text displayed: " + state);
+            return state;
+        } catch (Exception e) {
+            logger.info("Text 'Invalid username/password.' isn't displayed");
+            return false;
+        }
+    }
+}
+
+
+
