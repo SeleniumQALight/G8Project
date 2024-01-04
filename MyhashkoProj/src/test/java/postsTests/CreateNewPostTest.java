3b2216855600ae92e34b0b06644796c274e924ea
HruhoriiMyhashko <ssadhu1992@gmail.com>
2024-01-04 21:45:18 +0200

    Delete post part_1

--- MyhashkoProj/src/test/java/postsTests/CreateNewPostTest.java
+++ MyhashkoProj/src/test/java/postsTests/CreateNewPostTest.java
@@ -2,10 +2,12 @@ package postsTests;
 
 import baseTest.BaseTest;
 import libs.Util;
+import org.junit.After;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
     final String POST_TITLE = "TC_001_Myhashko " + Util.getDateAndTimeFormatted();
+
     @Test
     public void createNewPost() {
         pageProvider.loginPage()
@@ -27,11 +29,13 @@ public class CreateNewPostTest extends BaseTest {
         pageProvider.getPostPage().getHeader().clickOnButtonMyProfile()
                 .checkIsRedirectToMyProfilePage()
                 .checkPostWithTitleIsPresent(POST_TITLE)
-
         ;
-
-
-
     }
 
+    @After
+    public void deletePost() {
+        pageProvider.homePage()
+                .openHomePageAndLoginIfNeeded()
+        ;
+    }
 }
