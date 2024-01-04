406bde32f6ef43c4d93a4c1bc17dc8cf2c073c9b
KamaievaNataliia <nataliia.kamaeva@gmail.com>
2024-01-04 20:04:14 +0100

    added util.java and check post by title

--- KamaievaProj/src/test/java/postsTests/CreateNewPostTest.java
+++ KamaievaProj/src/test/java/postsTests/CreateNewPostTest.java
@@ -1,18 +1,21 @@
 package postsTests;
 
+import libs.Util;
 import BaseTest.BaseTest;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
 
+    final String POST_TITLE = "TC_001_Kamaieva" + Util.getDateAndTimeFormatted();
+
     @Test
-    public void createNewPost() {
+    public void TC_001_createNewPost() {
         pageProvider.getLoginPage()
                 .openLoginPageAndFillLoginFormWithValidCreat()
                 .checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoInputTitle("Kamaieva title")
+                .enterTextIntoInputTitle(POST_TITLE)
                 .enterTextIntoInputBody("Body text")
                 //.selectTextInDropdown("Приватне повідомлення")
                 .selectValueInDropdown("One Person")
@@ -20,5 +23,9 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToPostPage()
                 .checkIsSuccessMessageDisplayed()
                 .checkTextInSuccessMessage("New post successfully created.");
+
+        pageProvider.getPostPage().getHeader().clickOnButtonMyProfile()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE);
     }
 }
