25959d73fd4d12af0a2d71788d25047be2e8a0a7
Yuliia Petriv <yuli.petriv@gmail.com>
2024-01-04 21:04:14 +0200

    Util.java + check post by title

--- PetrivProj/src/test/java/postsTests/CreateNewPostTest.java
+++ PetrivProj/src/test/java/postsTests/CreateNewPostTest.java
@@ -1,17 +1,19 @@
 package postsTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
+    final String POST_TITLE = "TC_001_Petriv" + Util.getDateAndTimeFormatted();
     @Test
-    public void createNewPost() {
+    public void TC_001_createNewPost() {
         pageProvider.loginPage()
                 .openLoginPageAndFillLoginFormWithValidCred()
                 .checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoInputTitle("Petriv title")
+                .enterTextIntoInputTitle(POST_TITLE)
                 .enterTextIntoInputBody("body text")
                 //.selectTextInDropDown("Приватне повідомлення")
                 .selectValueInDropDown("One Person")
@@ -19,6 +21,11 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToPostPage()
                 .checkIsSuccessMessageDisplayed()
                 .checkTextInSuccessMessage("New post successfully created.")
+        ;
+
+        pageProvider.getPostPage().getHeader().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE)
         ;
     }
 }
