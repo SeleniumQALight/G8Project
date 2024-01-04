c73d5cc51bd358746de21717a2ce5e7d9fb28921
Bozhok-IA <152988814+Bozhok-IA@users.noreply.github.com>
2024-01-04 21:06:24 +0200

    Util.java + check post by title

--- BozhokProj/src/test/java/postsTests/CreateNewPostTest.java
+++ BozhokProj/src/test/java/postsTests/CreateNewPostTest.java
@@ -1,14 +1,16 @@
 package postsTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
+    final String POST_TITLE = "TC_001_Bozhok" + Util.getDateAndTimeFormatted();
     @Test
-    public void createNewPost() {
+    public void TC_001_createNewPost() {
         pageProvider.loginPage().openLoginPageAndFillLoginFormWithValidCred()
                 .checkIsRedirectOnHomePage().getHeader().clickOnButtonCreatePost()
-                .checkIsRedirectOnCreatePostPage().enterTitleInToInputTitle("Bozhok Title")
+                .checkIsRedirectOnCreatePostPage().enterTitleInToInputTitle(POST_TITLE)
                 .enterTextInToInputBody("Body text")
 //                .selectTextInToDropDown("Приватне повідомлення")
                 .selectValueInDropDown("One Person")
@@ -16,6 +18,10 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToPstPage()
                 .checkIsSuccesMassegeDisplayed()
                 .checkTextInSuccessMessage("New post successfully created.")
+        ;
+
+        pageProvider.getPostPage().getHeader().clickOnMyProfileButton()
+                .checkPostWithTitlesIsPresent(POST_TITLE)
                 ;
     }
 }
