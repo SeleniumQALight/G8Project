fe35d2c83b611fda3f85c5b36b1835aea2c370c8
IrynaOnofriichuk <irina1989ovcharenko@gmail.com>
2024-01-04 21:04:18 +0200

    Util.java + check post by title

--- OnofriichukProj/src/test/java/postsTests/CreateNewPostTest.java
+++ OnofriichukProj/src/test/java/postsTests/CreateNewPostTest.java
@@ -1,18 +1,20 @@
 package postsTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
+    final String POST_TITLE = "TC_001_onofriichuk" + Util.getDateAndTimeFormatted();
     @Test
 
-    public void createNewPost() {
+    public void TC_001_createNewPost() {
         pageProvider.loginPage()
                 .openLoginPageAndFillLoginWithValidCred()
                 .checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTitleInToInputTitle("onofriichuk title")
+                .enterTitleInToInputTitle(POST_TITLE)
                 .enterTextIntoInputBody("body text")
                 // .selectTextInDropDown("Приватне повідомлення")
                 .selectValueInDropDown("One Person")
@@ -20,6 +22,11 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToPostPage()
                 .checkIsSuccessMessageDisplayed()
                 .checkTextInSuccessMessage("New post successfully created.")
+        ;
+        pageProvider.getpostPage().getHeader().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE)
+
         ;
     }
 }
