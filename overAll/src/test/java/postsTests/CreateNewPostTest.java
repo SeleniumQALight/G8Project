cb6cf4442c53e97ecfe29cd57446a632b01cedcd
tradulenko <radulenko@gmail.com>
2024-01-04 21:04:15 +0200

    Util.java + check post by title

--- overAll/src/test/java/postsTests/CreateNewPostTest.java
+++ overAll/src/test/java/postsTests/CreateNewPostTest.java
@@ -1,17 +1,20 @@
 package postsTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
+    final String POST_TITLE = "TC_001_radulenko" + Util.getDateAndTimeFormatted();
+
     @Test
-    public void createNewPost() {
+    public void TC_001_createNewPost() {
         pageProvider.loginPage()
                 .openLoginPageAndFillLoginFormWithValidCred()
                 .checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTitleInToInputTitle("radulenko title")
+                .enterTitleInToInputTitle(POST_TITLE)
                 .enterTextIntoInputBody("body text")
                 //.selectTextInDropDown("Приватне повідомлення")
                 .selectValueInDropDown("One Person")
@@ -19,6 +22,11 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToPostPage()
                 .checkIsSuccessMessageDisplayed()
                 .checkTextInSuccessMessage("New post successfully created.")
+        ;
+
+        pageProvider.getPostPage().getHeader().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE)
         ;
     }
 }
