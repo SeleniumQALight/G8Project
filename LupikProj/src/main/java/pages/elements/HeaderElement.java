3732b618956a7e3586bbc94219b97b1f5e6456cf
Aleksandr Lupik <sashaloopik@gmail.com>
2024-01-04 21:04:16 +0200

    createNewPosTest part two

--- LupikProj/src/main/java/pages/elements/HeaderElement.java
+++ LupikProj/src/main/java/pages/elements/HeaderElement.java
@@ -5,12 +5,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pages.CommonActionsWithElements;
 import pages.CreatePostPage;
+import pages.MyProfilePage;
 
 // описує елементи які є в хедері залогіненого юзера
 public class HeaderElement extends CommonActionsWithElements {
     // create post button
     @FindBy(xpath = ".//*[@class='btn btn-sm btn-success mr-2']")
     private WebElement buttonCreatePost;
+
+    //my profile button
+    @FindBy(xpath = "//img[@alt='My profile']")
+    private WebElement buttonMyProfile;
 
     @FindBy(xpath = ".//button[text()='Sign Out']")
     private WebElement buttonSignOut;
@@ -29,5 +34,11 @@ public class HeaderElement extends CommonActionsWithElements {
     public CreatePostPage clickOnButtonCreatePost() {
         clickOnElement(buttonCreatePost);
         return new CreatePostPage(webDriver);
+
+
+    }
+    public MyProfilePage clickOnMyProfileButton() {
+        clickOnElement(buttonMyProfile);
+        return new MyProfilePage(webDriver);
     }
 }
