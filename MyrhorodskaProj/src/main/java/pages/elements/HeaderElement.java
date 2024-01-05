9aba6a1b22f9fb572d01f881ea1720263e7bb3b2
Myrhorodska <schatznata87@gmail.com>
2024-01-06 00:37:47 +0100

    HW-4 Myrhorodska added sign out test, registration test, changed login test

--- MyrhorodskaProj/src/main/java/pages/elements/HeaderElement.java
+++ MyrhorodskaProj/src/main/java/pages/elements/HeaderElement.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pages.CommonActionsWithElements;
 import pages.CreatePostPage;
+import pages.LoginPage;
 
 //описує елементи які є в хедері залогіненого юзера
 public class HeaderElement extends CommonActionsWithElements {
@@ -14,6 +15,26 @@ public class HeaderElement extends CommonActionsWithElements {
 
     @FindBy(xpath = "//button[contains(text(),'Sign Out')]")
     private WebElement buttonSignOut;
+
+
+    @FindBy(xpath = ".//span[@class='text-white mr-2']")
+    private WebElement userName;
+
+    @FindBy(xpath = ".//img[@data-original-title='My Profile']")
+    private WebElement buttonMyProfile;
+
+    @FindBy(xpath = ".//span[@data-original-title='Chat']")
+    private WebElement buttonChat;
+
+    @FindBy(xpath = ".//*[@data-original-title='Search']")
+    private WebElement buttonSearch;
+
+    public LoginPage clickOnButtonSignOut() {
+        clickOnElement(buttonSignOut);
+        return new LoginPage(webDriver);
+    }
+
+
     public HeaderElement(WebDriver webDriver) {
         super(webDriver);
     }
@@ -26,5 +47,50 @@ public class HeaderElement extends CommonActionsWithElements {
         clickOnElement(buttonCreatePost);
         return new CreatePostPage(webDriver);
     }
+    public boolean isButtonCreatePostVisible() {
+        return isElementDisplayed(buttonCreatePost);
+    }
+
+    public boolean isButtonMyProfileVisible() {
+        return isElementDisplayed(buttonMyProfile);
+    }
+
+    public boolean isUserNameVisible() {
+        return isElementDisplayed(userName);
+    }
+
+    public void checkInputInUserName(String text) {
+        checkTextInElement(userName, text);
+    }
+    public boolean isButtonChatVisible() {
+        return isElementDisplayed(buttonChat);
+    }
+
+    public boolean isButtonSearchVisible() {
+        return isElementDisplayed(buttonSearch);
+    }
+
+    public void isButtonSignOutNotVisible() {
+        checkIsElementNotVisible(buttonSignOut);
+    }
+
+    public void isButtonCreatePostNotVisible() {
+        checkIsElementNotVisible(buttonCreatePost);
+    }
+
+    public void isButtonMyProfileNotVisible() {
+        checkIsElementNotVisible(buttonMyProfile);
+    }
+
+    public void isButtonChatNotVisible() {
+        checkIsElementNotVisible(buttonChat);
+    }
+
+    public void isButtonSearchNotVisible() {
+        checkIsElementNotVisible(buttonSearch);
+    }
+
+
+
 }
 
--- MyrhorodskaProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ MyrhorodskaProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -16,7 +16,13 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.loginPage().enterTextInToInputLogin(VALID_LOGIN_UI);
         pageProvider.loginPage().enterTextInToInputPassword(VALID_PASSWORD_UI);
         pageProvider.loginPage().clickOnButtonSingIn();
-        Assert.assertTrue("Button SignOut is not visible",pageProvider.homePage().getHeader().isButtonSignOutVisible());;
+        Assert.assertTrue("Button Sign Out is not visible", pageProvider.homePage().getHeader().isButtonSignOutVisible());
+        Assert.assertTrue("Username is not visible", pageProvider.homePage().getHeader().isUserNameVisible());
+        Assert.assertTrue("Button Create Post is not visible", pageProvider.homePage().getHeader().isButtonCreatePostVisible());
+        Assert.assertTrue("Button MyProfile is not visible", pageProvider.homePage().getHeader().isButtonMyProfileVisible());
+        pageProvider.homePage().getHeader().checkInputInUserName(VALID_LOGIN_UI);
+        Assert.assertFalse("Input Username is visible", pageProvider.loginPage().isInputUsernameVisible());
+        Assert.assertFalse("Input Password is visible", pageProvider.loginPage().isInputPasswordVisible());
     }
     @Test
     public void invalidLogin() {
@@ -26,7 +32,7 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.loginPage().enterTextInToInputPassword("123456qwerty");
         pageProvider.loginPage().clickOnButtonSingIn();
 
-        Assert.assertFalse("Button 'Sign Out' is visible", pageProvider.homePage().isButtonSignOutVisible());
+        Assert.assertFalse("Button 'Sign Out' is visible", pageProvider.homePage().getHeader().isButtonSignOutVisible());
         Assert.assertTrue("Button 'Sign In' is not visible", pageProvider.loginPage().isButtonSignInVisible());
         Assert.assertTrue("Alert is not visible", pageProvider.loginPage().isInvalidUserNamePasswordMessageVisible());
 
