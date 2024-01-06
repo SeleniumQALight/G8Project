9aed9584693b26d74efe6e2ea237820b5b2e0eef
KamaievaNataliia <nataliia.kamaeva@gmail.com>
2024-01-07 00:09:52 +0100

    added checks to TC_001_createNewPost test

--- KamaievaProj/src/main/java/pages/CommonActionsWithElements.java
+++ KamaievaProj/src/main/java/pages/CommonActionsWithElements.java
@@ -49,15 +49,22 @@ public class CommonActionsWithElements {
     protected boolean isElementDisplayed(WebElement element) {
         try {
             boolean state = element.isDisplayed();
-            logger.info("Element" + getElementName(element) + " is displayed -> " + state);
+            String elementText = element.getText();
+            String logMessage;
+            if (!elementText.isEmpty()) {
+                logMessage = "Element '" + elementText + "' is displayed -> " + state;
+            } else {
+                logMessage = "Element '" + getElementName(element) + "' is displayed -> " + state;
+            }
+            logger.info(logMessage);
             return state;
         } catch (Exception e) {
             logger.error("Can not work with element");
             return false;
         }
     }
-    //select Text in dropdown
 
+    //select Text in dropdown
     protected void selectTextInDropdown(WebElement dropDown, String text) {
         try {
             Select select = new Select(dropDown);
@@ -90,6 +97,47 @@ public class CommonActionsWithElements {
         try {
             String textFromElement = element.getText();
             Assert.assertEquals("Text in element not matched", expectedText, textFromElement);
+        } catch (Exception e) {
+            logger.error("Can not work with element");
+            Assert.fail("Can not work with element");
+        }
+    }
+
+    protected void selectCheckbox(WebElement checkbox, String elementName) {
+        try {
+            if (!checkbox.isSelected()) {
+                checkbox.click();
+                logger.info(elementName + " was selected");
+            } else {
+                logger.info(elementName + " is already selected.");
+            }
+        } catch (Exception e) {
+            logger.error("Can not work with element");
+            Assert.fail("Can not work with element");
+        }
+    }
+
+    protected void unselectCheckbox(WebElement checkbox, String elementName) {
+        try {
+            if (checkbox.isSelected()) {
+                checkbox.click();
+                logger.info(elementName + " was unselected");
+            } else {
+                logger.info(elementName + " is already unselected.");
+            }
+        } catch (Exception e) {
+            logger.error("Can not work with element");
+            Assert.fail("Can not work with element");
+        }
+    }
+
+    protected void setCheckboxState(WebElement checkbox, String elementName, String state) {
+        try {
+            if (state.toLowerCase().equals("check")) {
+                selectCheckbox(checkbox, elementName);
+            } else if (state.toLowerCase().equals("uncheck")) {
+                unselectCheckbox(checkbox, elementName);
+            }
         } catch (Exception e) {
             logger.error("Can not work with element");
             Assert.fail("Can not work with element");
