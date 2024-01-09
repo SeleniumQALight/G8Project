6df0833bcb5fbbf297310d0462fbd593a07be8ae
IUAD0CVU <andrey.tepliuk@raiffeisen.ua>
2024-01-09 21:32:49 +0200

    ValidationMessagesTests part1

---
+++ TepliukProj/src/test/java/registrationTests/ValidationMessagesTests.java
@@ -0,0 +1,12 @@
+package registrationTests;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class ValidationMessagesTests extends BaseTest {
+    @Test
+    public void validationMessagesTest() {
+        pageProvider.loginPage().openLoginPage();
+        ;
+    }
+}
