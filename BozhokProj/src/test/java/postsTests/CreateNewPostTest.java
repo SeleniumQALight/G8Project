978d11e164e8cf5d80a596a6394e96712d6d9024
Bozhok-IA <152988814+Bozhok-IA@users.noreply.github.com>
2024-01-09 20:18:11 +0200

    delete post1

--- BozhokProj/src/test/java/postsTests/CreateNewPostTest.java
+++ BozhokProj/src/test/java/postsTests/CreateNewPostTest.java
@@ -17,7 +17,7 @@ public class CreateNewPostTest extends BaseTest {
 //                .selectTextInToDropDown("Приватне повідомлення")
                 .selectValueInDropDown("One Person")
                 .clickOnSaveNewButton()
-                .checkIsRedirectToPstPage()
+                .checkIsRedirectToPostPage()
                 .checkIsSuccesMassegeDisplayed()
                 .checkTextInSuccessMessage("New post successfully created.")
         ;
@@ -29,6 +29,10 @@ public class CreateNewPostTest extends BaseTest {
 
     @After
     public void deletePost() {
-        pageProvider.homePage().openHomePageAndLoginIfNeeded();
+        pageProvider.homePage().openHomePageAndLoginIfNeeded()
+                .getHeader().clickOnMyProfileButton()
+                .checkIsRedirectOnMyProfilePage()
+                .deletePostTillPresent(POST_TITLE)
+        ;
     }
 }
