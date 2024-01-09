ed3e285c47a0378038cd901cf2c462ef2bd4934c
tradulenko <radulenko@gmail.com>
2024-01-09 20:14:40 +0200

    delete post till resent

--- overAll/src/main/java/pages/PostPage.java
+++ overAll/src/main/java/pages/PostPage.java
@@ -11,6 +11,9 @@ public class PostPage extends ParentPage{
 
     private HeaderElement headerElement;
 
+    @FindBy(xpath = ".//button[@class='delete-post-button text-danger']")
+    private WebElement buttonDelete;
+
     public PostPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -34,5 +37,10 @@ public class PostPage extends ParentPage{
 
     public HeaderElement getHeader() {
         return headerElement = new HeaderElement(webDriver);
+    }
+
+    public MyProfilePage clickOnDeleteButton() {
+        clickOnElement(buttonDelete);
+        return new MyProfilePage(webDriver);
     }
 }
