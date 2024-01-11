b8c16dce82892e2d5ad4f36088176ab4e7e3b7e1
IrynaOnofriichuk <irina1989ovcharenko@gmail.com>
2024-01-11 22:06:02 +0200

    Invalid data to registration

--- OnofriichukProj/src/test/java/registrationTests/ValidationMessagesTests.java
+++ OnofriichukProj/src/test/java/registrationTests/ValidationMessagesTests.java
@@ -8,8 +8,10 @@ public class ValidationMessagesTests extends BaseTest {
     @Test
     @Ignore
     public void validationMessagesTest() {
-        //pageProvider.loginPage().enterTextIntoRegistrationUserNameField("test");
-       // pageProvider.loginPage().enterTextIntoRegistrationEmailField("trtr");
-        //pageProvider.loginPage().enterTextIntoRegistrationPasswordField("123456");
-        //pageProvider.loginPage().checkErrorsMessages("You must provide a valid email address."); Password must be a
+        pageProvider.loginPage();
+        pageProvider.loginPage().enterTextIntoRegistrationUserNameField("taras");
+        pageProvider.loginPage().enterTextIntoRegistrationEmailField("tr");
+        pageProvider.loginPage().enterTextIntoRegistrationPasswordField("tr");
+        pageProvider.loginPage().checkErrorsMessages(
+                "You must provide a valid email address.; Password must be at least 12 characters.");
 } }
