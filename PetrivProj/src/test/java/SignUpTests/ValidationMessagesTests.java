1c462d09e55db8447fdd16961a63e9a7f983aa5f
Yuliia Petriv <yuli.petriv@gmail.com>
2024-01-11 22:05:37 +0200

    ValidationMessagesTests part2

--- PetrivProj/src/test/java/SignUpTests/ValidationMessagesTests.java
+++ PetrivProj/src/test/java/SignUpTests/ValidationMessagesTests.java
@@ -1,17 +1,15 @@
 package SignUpTests;
 
 import baseTest.BaseTest;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ValidationMessagesTests extends BaseTest {
     @Test
-    @Ignore
     public void validationMessagesTest() {
-//        pageProvider.loginPage().openLoginPage();
-//        pageProvider.loginPage().enterTextIntoInputUserNameRegister("");
-//        pageProvider.loginPage().enterTextIntoInputEmailRegister("");
-//        pageProvider.loginPage().enterTextIntoInputPasswordRegister("");
-//        pageProvider.loginPage().checkErrorMessage("Username must be at least 3 characters.");
+        pageProvider.loginPage().openLoginPage();
+        pageProvider.loginPage().enterTextIntoRegistrationUserNameField("trsjd");
+        pageProvider.loginPage().enterTextIntoRegistrationEmailField("tr");
+        pageProvider.loginPage().enterTextIntoRegistrationPasswordField("tr");
+        pageProvider.loginPage().checkErrorMessages("You must provide a valid email address.;Password must be at least 12 characters.");
     }
 }
