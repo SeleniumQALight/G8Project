019e13447f049b4703f863a65b152a104c820bd9
tradulenko <radulenko@gmail.com>
2024-01-12 12:32:29 +0200

    ValidationMessagesTests part2

--- overAll/src/main/java/pages/CommonActionsWithElements.java
+++ overAll/src/main/java/pages/CommonActionsWithElements.java
@@ -5,15 +5,22 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CommonActionsWithElements {
     protected WebDriver webDriver;
     protected Logger logger = Logger.getLogger(getClass());
+    protected WebDriverWait webDriverWait10, webDriverWait15;
 
     public CommonActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this); // ініціалізує всі елементи сторінки опираючись на @FindBy
+        webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
     }
 
     protected void enterTextIntoInput(WebElement input, String text) {
@@ -37,6 +44,8 @@ public class CommonActionsWithElements {
 
     protected void clickOnElement(WebElement element) {
         try{
+            webDriverWait10
+                    .until(ExpectedConditions.elementToBeClickable(element));
             String elementName = getElementName(element);
             element.click();
             logger.info("Element was clicked " + elementName);
