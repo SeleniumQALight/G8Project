165e1a1d1a13d0fb527e941e82efb0b5fd418067
Olena Serhata <olena.serhata@pitchbook.com>
2024-01-13 21:32:39 +0200

    Home work 6, create new tests with methods from Actions class

--- SerhataProj/src/test/java/signUpTests/SignUpWithInvalidCredentials.java
+++ SerhataProj/src/test/java/signUpTests/SignUpWithInvalidCredentials.java
@@ -17,4 +17,19 @@ public class SignUpWithInvalidCredentials extends BaseTest {
         pageProvider.loginPage().checkIsValidationMessageForEmailFieldVisible();
         pageProvider.loginPage().checkIsValidationMessageForPasswordFieldVisible();
     }
+
+    @Test
+    public void invalidLoginUsingKeyboard() {
+        pageProvider.loginPage().openLoginPage();
+        pageProvider.loginPage().pressTabKeyNTimes(5);
+        pageProvider.loginPage().enterTextInToInputLogin(INVALID_DATA_FOR_SIGN_IN);
+        pageProvider.loginPage().pressTabKeyNTimes(1);
+        pageProvider.loginPage().enterTextInToInputEmail(INVALID_DATA_FOR_SIGN_IN);
+        pageProvider.loginPage().pressTabKeyNTimes(1);
+        pageProvider.loginPage().enterTextInToInputPasswordForSignUp(INVALID_DATA_FOR_SIGN_IN);
+        pageProvider.loginPage().pressEnterKey();
+        pageProvider.loginPage().checkIsValidationMessageForUserNameFieldVisible();
+        pageProvider.loginPage().checkIsValidationMessageForEmailFieldVisible();
+        pageProvider.loginPage().checkIsValidationMessageForPasswordFieldVisible();
+    }
 }
