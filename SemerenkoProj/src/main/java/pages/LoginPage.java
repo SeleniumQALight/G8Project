f63dc3906a95c866b7e1522c78e4ecc049005861
NickSemerenko <nickolay.sem@gmail.com>
2024-01-13 12:42:35 +0200

    Conflicts fix

--- SemerenkoProj/src/main/java/pages/LoginPage.java
+++ SemerenkoProj/src/main/java/pages/LoginPage.java
@@ -4,11 +4,13 @@ import libs.TestData;
 import libs.Util;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import pages.elements.HeaderElement;
 
 import java.util.ArrayList;
 import java.util.List;
