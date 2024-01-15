38999e1347079e30eea9225b7c4141c5928270c7
tymofiiyashchenko <tymofii.yashchenko@airslate.com>
2024-01-15 10:58:06 +0200

    HW6 Completed second part

--- YashchenkoProj/src/test/java/tabTests/TabTests.java
+++ YashchenkoProj/src/test/java/tabTests/TabTests.java
@@ -3,6 +3,9 @@ package tabTests;
 import baseTest.BaseTest;
 import org.junit.Test;
 
+import static libs.TestData.DEFAULT_VALID_LOGIN_UI;
+import static libs.TestData.DEFAULT_VALID_PASSWORD_UI;
+
 public class TabTests extends BaseTest {
 
     @Test
@@ -19,5 +22,15 @@ public class TabTests extends BaseTest {
         pageProvider.closeTab();
         pageProvider.switchToOriginalTab();
         pageProvider.homePage().getHeader().checkIsButtonSignOutVisible();
+    }
+
+    @Test
+    public void checkMissingOfNotSubmittedCredentials() {
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin(DEFAULT_VALID_LOGIN_UI);
+        pageProvider.getLoginPage().enterTextIntoInputPassword(DEFAULT_VALID_PASSWORD_UI);
+        pageProvider.refreshPage();
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+        pageProvider.homePage().getHeader().checkIsButtonSignOutNotVisible();
     }
 }
