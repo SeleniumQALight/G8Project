dfbc37d8d3dc88c602dd4cd09621d535133934e6
Aleksandr Lupik <sashaloopik@gmail.com>
2024-01-15 23:06:45 +0200

    delete

--- LupikProj/src/main/java/pages/ParrentPage.java
+++ LupikProj/src/main/java/pages/ParrentPage.java
@@ -30,47 +30,4 @@ abstract public class ParrentPage extends CommonActionsWithElements{
         );
     }
 
-    public void openLinkInNewTab() {
-        try {
-            ((JavascriptExecutor) webDriver).executeScript("window.open();");
-            logger.info("Link was opened in new tab");
-        } catch (Exception e) {
-            logger.error("Can not open link in new tab");
-            Assert.fail("Can not open link in new tab");
-        }
-    }
-
-    public void switchToTab(int i) {
-        try {
-            webDriver.switchTo().window(new ArrayList<>(webDriver.getWindowHandles()).get(i));
-            logger.info("Switched to tab #" + i);
-        } catch (Exception e) {
-            logger.error("Can not switch to tab #" + i);
-            Assert.fail("Can not switch to tab #" + i);
-        }
-
-
-    }
-
-    public void closeTab(int i) {
-
-        try {
-            webDriver.switchTo().window(new ArrayList<>(webDriver.getWindowHandles()).get(i));
-            webDriver.close();
-            logger.info("Tab #" + i + " was closed");
-        } catch (Exception e) {
-            logger.error("Can not close tab #" + i);
-            Assert.fail("Can not close tab #" + i);
-        }
-    }
-
-    public void refreshPage() {
-        try {
-            webDriver.navigate().refresh();
-            logger.info("Page was refreshed");
-        } catch (Exception e) {
-            logger.error("Can not refresh page");
-            Assert.fail("Can not refresh page");
-        }
-    }
 }
