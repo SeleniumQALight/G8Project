90a3654fc9325916f9748971eddbff3ba69d150a
tradulenko <radulenko@gmail.com>
2024-01-16 20:49:51 +0200

    test with params

--- overAll/src/test/java/registrationTests/ValidationMessagesTests.java
+++ overAll/src/test/java/registrationTests/ValidationMessagesTests.java
@@ -1,18 +1,33 @@
 package registrationTests;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class ValidationMessagesTests extends BaseTest {
-    @Test
-    public void validationMessagesTests() {
-        pageProvider.loginPage().openLoginPage();
-        pageProvider.loginPage().enterTextIntoRegistrationUserNameField("tr");
-        pageProvider.loginPage().enterTextIntoRegistrationEmailField("tr");
-        pageProvider.loginPage().enterTextIntoRegistrationPasswordField("tr");
-        pageProvider.loginPage().checkErrorsMessages(
-                "You must provide a valid email address.");
+    final String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final String ERROR_EMAIL = "You must provide a valid email address.";
+    final String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final String SEMICOLON = ";";
 
+    @Test
+    @Parameters(method = "parametersForValidationMessagesTests")
+    public void validationMessagesTests(String userName, String email, String password, String expectedMessages) {
+        pageProvider.loginPage().openLoginPage();
+        pageProvider.loginPage().enterTextIntoRegistrationUserNameField(userName);
+        pageProvider.loginPage().enterTextIntoRegistrationEmailField(email);
+        pageProvider.loginPage().enterTextIntoRegistrationPasswordField(password);
+        pageProvider.loginPage().checkErrorsMessages(expectedMessages);
+    }
+
+    public Object[][] parametersForValidationMessagesTests() {
+        return new Object[][]{
+                {"taras", "tr", "tr", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"taras", "tr@tr.com", "tr", ERROR_PASSWORD}
+        };
     }
 
 }
