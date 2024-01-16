e8c8922a5b301b50a269f892970c2e12c46a19f9
Nikita <qa.nikita.p@alldigitalads.com>
2024-01-16 20:50:07 +0200

    test with params

--- PavlykProj/src/test/java/registratinTests/ValidationMessagesTests.java
+++ PavlykProj/src/test/java/registratinTests/ValidationMessagesTests.java
@@ -1,17 +1,34 @@
 package registratinTests;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class ValidationMessagesTests extends BaseTest {
 
-    @Test
-    public void validationMessagesTests() {
-        pageProvider.loginPage().openLoginPage();
-        pageProvider.loginPage().enterTextIntoInputUsernameRegistration("taras");
-        pageProvider.loginPage().enterTextIntoInputEmailRegistration("tr");
-        pageProvider.loginPage().enterTextIntoInputPasswordRegistration("tr");
-        pageProvider.loginPage().checkErrorsMessages("You must provide a valid email address.;Password must be at least 12 characters.");
 
+    final String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final String ERROR_EMAIL = "You must provide a valid email address.";
+    final String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final String SEMICOLON = ";";
+
+    @Test
+    @Parameters(method = "parametersForValidationMessagesTests")
+    public void validationMessagesTests(String userName, String email, String password, String expectedMessage) {
+        pageProvider.loginPage().openLoginPage();
+        pageProvider.loginPage().enterTextIntoInputUsernameRegistration(userName);
+        pageProvider.loginPage().enterTextIntoInputEmailRegistration(email);
+        pageProvider.loginPage().enterTextIntoInputPasswordRegistration(password);
+        pageProvider.loginPage().checkErrorsMessages(expectedMessage);
+    }
+
+    public Object[][] parametersForValidationMessagesTests() {
+        return new Object[][]{
+                {"taras", "tr", "tr", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"taras", "tr@tr.cm", "tr", ERROR_PASSWORD},
+        };
     }
 }
