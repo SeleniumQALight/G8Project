ff5ef1f0f13449fb6e608bcc5fee092f84bc8cbc
Aleksandr Lupik <sashaloopik@gmail.com>
2024-01-16 20:50:14 +0200

    paremeters

--- LupikProj/src/test/java/signUpTest/ValidationMessagesTests.java
+++ LupikProj/src/test/java/signUpTest/ValidationMessagesTests.java
@@ -2,19 +2,33 @@ package signUpTest;
 
 import baseTest.BaseTest;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+   @RunWith(JUnitParamsRunner.class)
 public class ValidationMessagesTests extends BaseTest {
-
+    final String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final String ERROR_EMAIL = "You must provide a valid email address.";
+    final String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final String SEMICOLON = ";";
     @Test
-
-    public void validationMessagesTest() {
+       @Parameters(method = "parameterForValidationMessageTests")
+       public void validationMessagesTest( String userName, String email,String password, String expectedMessage) {
               pageProvider.loginPage().openLoginPage();
-              pageProvider.loginPage().enterTextIntoRegisterLogin("taras");
-              pageProvider.loginPage().enterTextIntoRegisterEmail("tr");
-              pageProvider.loginPage().enterTextIntoRegisterPassword("tr");
-            pageProvider.loginPage().checkErrorMessages("You must provide a valid email address.;Password must be at least 12 characters.");
+              pageProvider.loginPage().enterTextIntoRegisterLogin(userName);
+              pageProvider.loginPage().enterTextIntoRegisterEmail(email);
+              pageProvider.loginPage().enterTextIntoRegisterPassword(password);
+            pageProvider.loginPage().checkErrorMessages(expectedMessage);
 
 
+    }
+
+    public Object[][] parameterForValidationMessageTests() {
+        return new Object[][]{
+                {"taras", "tr", "tr", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"taras", "tr@tr.com", "tr", ERROR_PASSWORD}
+        };
     }
 }
