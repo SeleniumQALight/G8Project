b642a940bae1468b34303c8ce0de528359a029f5
tymofiiyashchenko <tymofii.yashchenko@airslate.com>
2024-01-16 20:49:51 +0200

    test with params

--- YashchenkoProj/src/test/java/registrationTest/ValidationMessagesTest.java
+++ YashchenkoProj/src/test/java/registrationTest/ValidationMessagesTest.java
@@ -1,15 +1,32 @@
 package registrationTest;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class ValidationMessagesTest extends BaseTest {
+    final String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final String ERROR_EMAIL = "You must provide a valid email address.";
+    final String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final String SEMICOLON = ";";
+
     @Test
-    public void validationMessagesTest() {
+    @Parameters(method = "parametersForValidationMessagesTests")
+    public void validationMessagesTest(String userName, String email, String password, String expectedMessages) {
         pageProvider.getLoginPage().openLoginPage();
-        pageProvider.getLoginPage().enterTextIntoRegistrationInputLogin("Tim");
-        pageProvider.getLoginPage().enterTextIntoRegistrationInputEmail("123");
-        pageProvider.getLoginPage().enterTextIntoRegistrationInputPassword("123");
-        pageProvider.getLoginPage().checkErrorMessages("Password must be at least 12 characters.;You must provide a valid email address.");
+        pageProvider.getLoginPage().enterTextIntoRegistrationInputLogin(userName);
+        pageProvider.getLoginPage().enterTextIntoRegistrationInputEmail(email);
+        pageProvider.getLoginPage().enterTextIntoRegistrationInputPassword(password);
+        pageProvider.getLoginPage().checkErrorMessages(expectedMessages);
+    }
+
+    public Object[][] parametersForValidationMessagesTests() {
+        return new Object[][]{
+                {"tim", "123", "123", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"tim", "tim@gmail.com", "123", ERROR_PASSWORD}
+        };
     }
 }
