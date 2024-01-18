2994fc74343edf3e69701405b7b1fe42e3b6e6de
tradulenko <radulenko@gmail.com>
2024-01-18 21:18:33 +0200

    ExcelDriver

--- overAll/src/test/java/loginTests/LoginTestWithPageObject.java
+++ overAll/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,11 +1,16 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import libs.ConfigProvider;
+import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static libs.TestData.VALID_LOGIN_UI;
-import static libs.TestData.VALID_PASSWORD_UI;
+import java.io.IOException;
+import java.util.Map;
+
+import static data.TestData.VALID_LOGIN_UI;
+import static data.TestData.VALID_PASSWORD_UI;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
@@ -18,5 +23,19 @@ public class LoginTestWithPageObject extends BaseTest {
         Assert.assertTrue("Button SignOut is not visible",
                 pageProvider.homePage().getHeader().isButtonSignOutVisible());
     }
+
+    @Test
+    public void validLoginWithExcel() throws IOException {
+        Map<String, String> dataForValidLogin =
+                ExcelDriver.getData(ConfigProvider.configProperties.DATA_FILE(), "validLogOn");
+        pageProvider.loginPage().openLoginPage();
+        pageProvider.loginPage().enterTextIntoInputLogin(dataForValidLogin.get("login"));
+        pageProvider.loginPage().enterTextIntoInputPassword(dataForValidLogin.get("pass"));
+        pageProvider.loginPage().clickOnButtonSingIn();
+
+        Assert.assertTrue("Button SignOut is not visible",
+                pageProvider.homePage().getHeader().isButtonSignOutVisible());
+    }
+
 
 }
---
+++ overAll/src/test/java/registrationTests/ValidationMessagesTestsWithExcel.java
@@ -0,0 +1,38 @@
+package registrationTests;
+
+import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import libs.ConfigProvider;
+import libs.ExcelSpreadsheetData;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+
+@RunWith(JUnitParamsRunner.class)
+public class ValidationMessagesTestsWithExcel extends BaseTest {
+
+
+    @Test
+    @Parameters(method = "parametersForValidationMessagesTests")
+    public void validationMessagesTests(String userName, String email, String password, String expectedMessages) {
+        pageProvider.loginPage().openLoginPage();
+        pageProvider.loginPage().enterTextIntoRegistrationUserNameField(userName);
+        pageProvider.loginPage().enterTextIntoRegistrationEmailField(email);
+        pageProvider.loginPage().enterTextIntoRegistrationPasswordField(password);
+        pageProvider.loginPage().checkErrorsMessages(expectedMessages);
+    }
+
+    public Collection parametersForValidationMessagesTests() throws IOException {
+        final String pathToDataFile = ConfigProvider.configProperties.DATA_FILE_PATH() + "testDataSuit.xls";
+        final String sheetName = "registrationErrors";
+        final boolean skipFirstRow = false; // if true - skip first row in excel file
+        logger.info("Data file path: " + pathToDataFile + "\nSheet name: " + sheetName + "\nSkip first row: " + skipFirstRow);
+
+       return new ExcelSpreadsheetData(new FileInputStream(pathToDataFile), sheetName, skipFirstRow).getData();
+    }
+
+}
