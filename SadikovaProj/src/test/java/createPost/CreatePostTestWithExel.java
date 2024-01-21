19ae14d33a0db57cf9f750f18b5c6d56bac9daca
qa.maria.s <qa.maria.s@alldigitalads.com>
2024-01-21 18:05:46 +0100

    hw8

--- SadikovaProj/src/test/java/createPost/CreatePostTestWithExel.java
+++ SadikovaProj/src/test/java/createPost/CreatePostTestWithExel.java
@@ -6,38 +6,49 @@ import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import libs.ConfigProvider;
 import libs.ExcelSpreadsheetData;
+import libs.Util;
 import org.junit.After;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Date;
+
+import static org.bouncycastle.asn1.x500.style.RFC4519Style.description;
 
 @RunWith(JUnitParamsRunner.class)
 public class CreatePostTestWithExel extends BaseTest {
 
+    String POST_TITLE;
+    String POST_BODY;
+    String DATA = Util.getDateAndTimeFormattedOnlyNumbers();
+
 
     @Test
     @Parameters(method = "parametersForCreatePost")
-    public void createPost() {
+    public void createPost(String postTitle, String textBody, String dropdownValue, String selectCheckbox, String notification, String webElementValue, String testNumber) {
+        POST_TITLE = String.format(postTitle, DATA, testNumber);
+        POST_BODY = String.format(textBody, DATA);
         pageProvider.mainPage()
                 .loginToProfile(TestData.VALID_LOGIN_UI, TestData.VALID_PASSWORD);
         pageProvider.homePage().checkIsRedirectToHomePage();
         pageProvider.headerElement().clickCreatePostButton();
         pageProvider.createPostPage().checkIsRedirectToCreatePostPage();
-        pageProvider.createPostPage().enterTextInTitleInInputTitle(postTitle);
-        pageProvider.createPostPage().enterTextInBodyContentField(TEXT_BODY);
-        pageProvider.createPostPage().selectValueInDropdown(ONE_PERSON);
-        pageProvider.createPostPage().checkSetStateCheckbox("check");
+        pageProvider.createPostPage().enterTextInTitleInInputTitle(POST_TITLE);
+        pageProvider.createPostPage().enterTextInBodyContentField(POST_BODY);
+        pageProvider.createPostPage().selectValueInDropdown(dropdownValue);
+        pageProvider.createPostPage().checkSetStateCheckbox(selectCheckbox);
         pageProvider.createPostPage().clickSaveNewPostButton();
         pageProvider.postPage().checkIsSuccessMessageDisplayes();
-        pageProvider.postPage().checkTextPresent("New post successfully created.")
-                .checkTitleTextIsEquals(postTitle)
-                .checkBodyTextIsEquals(TEXT_BODY)
-                .checkThisIsPostUniqueWebElementIsPresent("yes")
-                .checkNoteBlockWebElementIsPresent(ONE_PERSON);
+        pageProvider.postPage().checkTextPresent(notification)
+                .checkTitleTextIsEquals(POST_TITLE)
+                .checkBodyTextIsEquals(POST_BODY)
+                .checkThisIsPostUniqueWebElementIsPresent(webElementValue)
+                .checkNoteBlockWebElementIsPresent(dropdownValue);
 
 
         pageProvider.postPage().getHeaderElement()
@@ -59,8 +70,8 @@ public class CreatePostTestWithExel extends BaseTest {
 
     public Collection parametersForCreatePost() throws IOException {
         final String pathToDataFile = ConfigProvider.configProperties.DATA_FILE_PATH() + "testDataSuit.xls";
-        final String sheetName = "registrationErrors";
-        final boolean skipFirstRow = true; //if true - skip first row inn excel file
+        final String sheetName = "sadikova";
+        final boolean skipFirstRow = false; //if true - skip first row inn excel file
         logger.info("Data file patch" + pathToDataFile + "\nSheet name: " + sheetName + "\n Skip first row: " + skipFirstRow);
         return new ExcelSpreadsheetData(new FileInputStream(pathToDataFile), sheetName, skipFirstRow).getData();
 
