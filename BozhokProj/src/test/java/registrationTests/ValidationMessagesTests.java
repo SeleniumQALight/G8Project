e95d849f156dcfa3ca42b5e0d60f6582015575f9
Bozhok-IA <152988814+Bozhok-IA@users.noreply.github.com>
2024-01-21 14:55:38 +0200

    Errors message in registration

--- BozhokProj/src/test/java/registrationTests/ValidationMessagesTests.java
+++ BozhokProj/src/test/java/registrationTests/ValidationMessagesTests.java
@@ -11,6 +11,10 @@ public class ValidationMessagesTests extends BaseTest {
     final String ERROR_EMAIL = "You must provide a valid email address.";
     final String ERROR_PASSWORD = "Password must be at least 12 characters.";
     final String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final String ERROR_USERNAME_TAKEN = "This username is already taken.";
+    final String ERROR_UNVALID_EMAIL = "You must provide a valid email address.";
+    final String ERROR_USERNAME_ONLY_LETTERS = "Username can only contain letters and numbers.";
+    final String ERROR_PASSWORD_MORE_60_SYMBOLS = "Password cannot exceed 50 characters.";
     final String SEMICOLON = ";";
 
     @Test
@@ -26,7 +30,22 @@ public class ValidationMessagesTests extends BaseTest {
     public Object[][] parametersForValidationMessageTests() {
         return new Object[][]{
                 {"taras", "tr", "tr", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
-                {"taras", "tr@tr.com", "tr", ERROR_PASSWORD}
+                {"taras", "tr@tr.com", "tr", ERROR_PASSWORD},
+                {"tr", "tr@tr.com", "tr", ERROR_USERNAME + SEMICOLON + ERROR_PASSWORD},
+                {"qaauto", "tr@tr.com", "tr", ERROR_USERNAME_TAKEN + SEMICOLON
+                        + ERROR_PASSWORD},
+                {"qaauto", "trtr.com", "tr", ERROR_USERNAME_TAKEN + SEMICOLON + ERROR_UNVALID_EMAIL + SEMICOLON
+                        + ERROR_PASSWORD},
+                {"taras", "trtr", "tr", ERROR_UNVALID_EMAIL + SEMICOLON
+                        + ERROR_PASSWORD},
+                {"тест", "trtr", "tr", ERROR_USERNAME_ONLY_LETTERS + SEMICOLON
+                        + ERROR_UNVALID_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"taras", "tr@tr.com", "trtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtr",
+                        ERROR_PASSWORD_MORE_60_SYMBOLS},
+                {"tr", "tr@tr.com", "trtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtrtr",
+                        ERROR_USERNAME + SEMICOLON + ERROR_PASSWORD_MORE_60_SYMBOLS},
+
+
         };
     }
 }
