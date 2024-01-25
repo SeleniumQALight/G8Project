e18b70d56b73ea857815c1bb8388444bfb8ecebc
Hryhorii77 <serhii.diachenko75@gmail.com>
2024-01-25 20:58:49 +0200

    browsers

--- PrudkyiProj/src/main/java/pages/ParentPage.java
+++ PrudkyiProj/src/main/java/pages/ParentPage.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.WebDriver;
 
 //all general methods for all pages
 public class ParentPage extends CommonActionsWithElements {
-    final String baseUrl = ConfigProvider.configProperties.base_url();
+   String env = System.getProperty("env", "qa");
+    final String baseUrl = ConfigProvider.configProperties.base_url().replace("env", env);
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
     }
