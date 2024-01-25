f8a4cf96fd1f346b2e55d59172a22f68a923c43a
Olesia Storozhuk <fneerkl@gmail.com>
2024-01-25 19:57:35 +0200

    hiddenConfig

--- StorozhukProj/src/main/java/data/TestData.java
+++ StorozhukProj/src/main/java/data/TestData.java
@@ -1,9 +1,11 @@
 package data;
 
-// this class is for storing test data
+import libs.ConfigProvider;
+
+// this class is for saving test data
 //which is used in tests many times
 public class TestData {
-    public final static String VALID_LOGIN_UI = "qaauto";
+    public final static String VALID_LOGIN_UI = System.getProperty("defaultLogin", ConfigProvider.configHiddenProperties.login());
     public final static String VALID_PASSWORD_UI = "123456qwerty";
 
 }
