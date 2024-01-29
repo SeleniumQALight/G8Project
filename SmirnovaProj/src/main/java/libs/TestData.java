fc157697ecef106b0b7102e2d90513a2fb6c7e9d
a.smirnova <nastyanastina9@gmail.com>
2024-01-29 11:30:20 +0100

    hiddenConfig

--- SmirnovaProj/src/main/java/libs/TestData.java
+++ SmirnovaProj/src/main/java/libs/TestData.java
@@ -3,7 +3,8 @@ package libs;
 //клас для зберігання тестових даних
 //які будуть використовуватись в тестах БАГАТО РАЗІВ
 public class TestData {
-    public final static String VALID_LOGIN_UI = "qaauto";
+    public final static String VALID_LOGIN_UI = System.getProperty
+            ("defaultLogin", ConfigProvider.configHiddenProperties.login());
     public final static String VALID_PASSWORD_UI = "123456qwerty";
     public final static String INVALID_SIGNUP_UI = "tr";
 }
