797fcdf6fbcbd697eb57aba2d692e3bc2142af40
HruhoriiMyhashko <ssadhu1992@gmail.com>
2024-01-30 21:45:14 +0200

    Allure

--- MyhashkoProj/src/test/java/baseTest/BaseTest.java
+++ MyhashkoProj/src/test/java/baseTest/BaseTest.java
@@ -3,10 +3,16 @@ package baseTest;
 // Parent class for all tests
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
+import libs.ScreenShot;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,13 +22,16 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import pages.PageProvider;
 
+import java.io.ByteArrayInputStream;
 import java.time.Duration;
+import java.util.ArrayList;
 
 public class BaseTest {
     WebDriver webDriver;
     protected Logger logger = Logger.getLogger(getClass());
     protected PageProvider pageProvider;
 
+    protected ArrayList<ScreenShot> listOfScreenShots = new ArrayList<>();
     // Will be executed before each test
     @Before
     public void setUp() {
@@ -36,39 +45,80 @@ public class BaseTest {
         pageProvider = new PageProvider(webDriver);
     }
 
-    @After
-    public void tearDown() {
-        webDriver.quit(); // Close browser
-        logger.info("Browser closed");
-        logger.info("------ " + testName.getMethodName() + " was ended ------");
-    }
-
-
+    //    @After
+//    public void tearDown() {
+//        webDriver.quit(); // Close browser
+//        logger.info("Browser closed");
+//        logger.info("------ " + testName.getMethodName() + " was ended ------");
+//    }
+//
     @Rule
     public TestName testName = new TestName();
 
-    private WebDriver initDriver(){
-        String browser = System.getProperty("browser");
-        if ((browser == null) || ("chrome".equals(browser.toLowerCase()))){ // default browser -Dbrowser=chrome
-            WebDriverManager.chromedriver().setup();
-            webDriver = new ChromeDriver();
-        } else if ("firefox".equals(browser.toLowerCase())){ // -Dbrowser=firefox
-            WebDriverManager.firefoxdriver().setup();
-            webDriver = new FirefoxDriver();
-        } else if ("ie".equals(browser.toLowerCase())){ // -Dbrowser=ie
-            WebDriverManager.iedriver().setup(); //zoom 100%
-            webDriver = new InternetExplorerDriver(); //security level - Medium
-        } else if ("safari".equalsIgnoreCase(browser)) {// -Dbrowser=safari
-            WebDriverManager.safaridriver().setup();
-            webDriver = new SafariDriver();
-        } else if ("edge".equalsIgnoreCase(browser)) { // -Dbrowser=edge
-            WebDriverManager.edgedriver().setup();
-            webDriver = new EdgeDriver();
-        }  else {
-            throw new IllegalArgumentException("Browser " + browser + " is not supported");
+    @Rule()
+    public final TestWatcher watchman = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            screenshot();
         }
-        return webDriver;
+
+        public void saveScreenshot(ArrayList<ScreenShot> screenShots) {
+            screenShots.forEach(screenShot -> Allure.addAttachment(screenShot.getName(),
+                    new ByteArrayInputStream(screenShot.getScreenShotImg())));
+        }
+
+        public void screenshot() {
+            if (webDriver == null) {
+                logger.info("Driver for screenshot not found");
+                return;
+            }
+            byte[] screen = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+            listOfScreenShots.add(new ScreenShot("Default screenShot after failed test", screen));
+            saveScreenshot(listOfScreenShots);
+        }
+
+
+        @Override
+        protected void finished(Description description) {
+            logger.info(
+                    String.format("Finished test: %s::%s", description.getClassName(), description.getMethodName()));
+            try {
+                webDriver.quit();
+                logger.info("Browser was closed");
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+    };
+
+    protected void takeScreenshot(String screenShotName) {
+        System.out.println("screenshot was taken");
+        byte[] screen = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+        listOfScreenShots.add(new ScreenShot(screenShotName, screen));
     }
-}
+
+        private WebDriver initDriver() {
+            String browser = System.getProperty("browser");
+            if ((browser == null) || ("chrome".equals(browser.toLowerCase()))) { // default browser -Dbrowser=chrome
+                WebDriverManager.chromedriver().setup();
+                webDriver = new ChromeDriver();
+            } else if ("firefox".equals(browser.toLowerCase())) { // -Dbrowser=firefox
+                WebDriverManager.firefoxdriver().setup();
+                webDriver = new FirefoxDriver();
+            } else if ("ie".equals(browser.toLowerCase())) { // -Dbrowser=ie
+                WebDriverManager.iedriver().setup(); //zoom 100%
+                webDriver = new InternetExplorerDriver(); //security level - Medium
+            } else if ("safari".equalsIgnoreCase(browser)) {// -Dbrowser=safari
+                WebDriverManager.safaridriver().setup();
+                webDriver = new SafariDriver();
+            } else if ("edge".equalsIgnoreCase(browser)) { // -Dbrowser=edge
+                WebDriverManager.edgedriver().setup();
+                webDriver = new EdgeDriver();
+            } else {
+                throw new IllegalArgumentException("Browser " + browser + " is not supported");
+            }
+            return webDriver;
+        }
+    }
 
 
