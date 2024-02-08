bbacfd45183b901cb863f62c9f26904c6494b7ef
Yuliia Petriv <yuli.petriv@gmail.com>
2024-02-08 21:50:58 +0200

    ApiTests.java

---
+++ PetrivProj/src/test/java/apiTests/ApiTests.java
@@ -0,0 +1,24 @@
+package apiTests;
+
+import api.EndPoints;
+import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTests {
+    final String USER_NAME = "autoapi";
+    Logger logger = Logger.getLogger(getClass());
+    @Test
+    public void getAllPostsByUser() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all() // print request to console
+            .when()
+                .get(EndPoints.POSTS_BY_USER, USER_NAME) // URL
+            .then()
+                .log().all() // print response to console
+                .statusCode(200);
+    }
+}
