0d42815f9ab1cddde5465738a647badef9c6ddb3
Vladyslava Sushko <vladyslava@mppteam.com>
2024-02-12 12:42:37 +0200

    getAllPostsByUser api

---
+++ SushkoProj/src/main/java/api/EndPoints.java
@@ -0,0 +1,6 @@
+package api;
+
+public class EndPoints {
+    public static String BASE_URL = "https://aqa-complexapp.onrender.com";
+    public static String POSTS_BY_USER = BASE_URL + "/api/postsByAuthor/{0}";
+}
