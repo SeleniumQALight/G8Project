2598dd8c76122948e1cfc66d78c231dab670d1f4
KamaievaNataliia <nataliia.kamaeva@gmail.com>
2024-02-13 20:24:42 +0100

    first part

--- KamaievaProj/src/test/java/apiTests/ApiTests.java
+++ KamaievaProj/src/test/java/apiTests/ApiTests.java
@@ -1,11 +1,18 @@
 package apiTests;
 
 import api.EndPoints;
+import api.dto.responseDto.AuthorDTO;
+import api.dto.responseDto.PostsDto;
 import io.restassured.http.ContentType;
 import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
 
 public class ApiTests {
     final String USER_NAME = "autoapi";
@@ -13,13 +20,43 @@ public class ApiTests {
 
     @Test
     public void getAllPostsByUser() {
-        given()
-                .contentType(ContentType.JSON)
-                .log().all()
+       PostsDto[] actualResponseAsDto =
+               given()
+                       .contentType(ContentType.JSON)
+                       .log().all()
                 .when()
-                .get(EndPoints.POSTS_BY_USER, USER_NAME)
+                       .get(EndPoints.POSTS_BY_USER, USER_NAME)
                 .then()
-                .log().all()
-                .statusCode(200);
+                    .log().all()
+                    .statusCode(SC_OK)
+                    .assertThat()
+                    .body("[0].title", equalTo("test2"))
+                    .body("author.username",everyItem(equalTo(USER_NAME)))
+                    .extract().body().as(PostsDto[].class);
+       logger.info(actualResponseAsDto[0].toString());
+       logger.info("Size = " + actualResponseAsDto.length);
+       logger.info("Title [0] = " + actualResponseAsDto[0].getTitle());
+       logger.info("UserName [0] = " + actualResponseAsDto[0].getAuthor().getUsername());
+
+        for (int i = 0; i < actualResponseAsDto.length; i++) {
+            Assert.assertEquals("Username is not matched in post " + i, USER_NAME, actualResponseAsDto[i].getAuthor().getUsername());
+        }
+
+        //Expected Result
+        PostsDto[] expectedDto = {
+                new PostsDto("test2", "test body2", "All Users", "no", new AuthorDTO(USER_NAME), false),
+                new PostsDto("test", "test body", "All Users", "no", new AuthorDTO(USER_NAME), false)
+        };
+
+        Assert.assertEquals("Number of posts ", expectedDto.length, actualResponseAsDto.length);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.
+                assertThat(actualResponseAsDto)
+                        .usingRecursiveComparison()
+                .ignoringFields("id", "createdDate","author.avatar")
+                                .isEqualTo(expectedDto);
+        softAssertions.assertAll();
+
     }
 }
