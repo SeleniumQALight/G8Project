8260ab75e9c0a2c140ca087d817dd497eb3cd35c
Anton Kraynov <qa.anton.k@alldigitalads.com>
2024-02-15 22:22:59 +0200

    work with negative tests

--- KraynovProj/src/main/java/data/TestData.java
+++ KraynovProj/src/main/java/data/TestData.java
@@ -8,4 +8,7 @@ public class TestData {
     public final static String VALID_LOGIN_UI = System.getProperty("defaultLogin", ConfigProvider.configHiddenProperties.login());
     public final static String VALID_PASSWORD_UI = "123456qwerty";
     public final static String SMALL_INVALID_TEXT = "tr";
+    public final static String VALID_LOGIN_API = "qaanton";
+    public final static String VALID_PASSWORD_API = "qwertyui123";
+
 }
