5536c76b2c134d7aefb37dc4b4d1ba1236aaf527
Yuliia Petriv <yuli.petriv@gmail.com>
2024-02-15 21:51:12 +0200

    CreatePost part1

--- PetrivProj/src/main/java/api/ApiHelper.java
+++ PetrivProj/src/main/java/api/ApiHelper.java
@@ -1,15 +1,19 @@
 package api;
 
 
+import data.TestData;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import static io.restassured.RestAssured.given;
 
@@ -37,5 +41,27 @@ public class ApiHelper {
 
     public ValidatableResponse getAllPostsByUserRequest(String userName) {
         return getAllPostsByUserRequest(userName, HttpStatus.SC_OK);
+    }
+
+    public String getToken(String userName, String password) {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("username", userName);
+        requestBody.put("password", password);
+
+        ResponseBody responseBody =
+                given()
+                        .spec(requestSpecification)
+                        .body(requestBody.toMap())
+                        .when()
+                        .post(EndPoints.LOGIN) // URL
+                        .then()
+                        .spec(responseSpecification)
+                        .extract().response().getBody();
+
+        return responseBody.asString().replace("\"", "");
+    }
+
+    public String getToken() {
+        return getToken(TestData.VALID_LOGIN_API, TestData.VALID_PASSWORD_API);
     }
 }
--- PetrivProj/src/main/java/data/TestData.java
+++ PetrivProj/src/main/java/data/TestData.java
@@ -6,4 +6,8 @@ import libs.ConfigProvider;
 public class TestData {
     public final static String VALID_LOGIN_UI = System.getProperty("defaultLogin", ConfigProvider.configHiddenProperties.login());
     public final static String VALID_PASSWORD_UI = "123456qwerty";
+
+    public final static String VALID_LOGIN_API = "yuliiaapi";
+    public final static String VALID_PASSWORD_API = "123456qwerty";
+
 }
