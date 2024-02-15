6d55949868f5b7ec6ce1fbf4929f0e2498ec2a1c
HruhoriiMyhashko <ssadhu1992@gmail.com>
2024-02-15 21:20:25 +0200

    Api tests

--- MyhashkoProj/src/main/java/api/dto/responseDto/AuthorDTO.java
+++ MyhashkoProj/src/main/java/api/dto/responseDto/AuthorDTO.java
@@ -1,18 +1,17 @@
 package api.dto.responseDto;
 
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Data
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthorDTO {
     private String username;
     private String avatar;
 
-    public AuthorDTO(String username) {
-        this.username = username;
-    }
+//    public AuthorDTO(String username) {
+//    }
 }
