e4dfdd988edcfa70e3fdfcce64d529b3e940e5ac
Olena Serhata <olena.serhata@pitchbook.com>
2024-02-15 21:51:15 +0200

    First part new api test Create Post

---
+++ SerhataProj/src/test/java/apiTests/CreatePostByApiTest.java
@@ -0,0 +1,23 @@
+package apiTests;
+
+import api.ApiHelper;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CreatePostByApiTest {
+    String token;
+    ApiHelper apiHelper = new ApiHelper();
+
+    @Before
+    public void deletePosts() {
+         token = apiHelper.getToken();
+         System.out.println("--" + token + "--");
+    }
+
+    @Ignore
+    @Test
+    public void createPostByApi() {
+
+    }
+}
