fec7b421727edc654ae0629adfdf164fddbd6aed
Yuliia Petriv <yuli.petriv@gmail.com>
2024-02-15 21:20:15 +0200

    Api tests

---
+++ PetrivProj/src/main/java/api/ApiHelper.java
@@ -0,0 +1,41 @@
+package api;
+
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiHelper {
+    Logger logger = Logger.getLogger(getClass());
+
+    RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+            .log(LogDetail.ALL)
+            .expectStatusCode(HttpStatus.SC_OK)
+            .build();
+
+    public ValidatableResponse getAllPostsByUserRequest(String userName, int statusCode) {
+        return given()
+                .spec(requestSpecification)
+                .when()
+                .get(EndPoints.POSTS_BY_USER, userName)
+                .then()
+                .spec(responseSpecification.statusCode(statusCode));
+    }
+
+    public ValidatableResponse getAllPostsByUserRequest(String userName) {
+        return getAllPostsByUserRequest(userName, HttpStatus.SC_OK);
+    }
+}
