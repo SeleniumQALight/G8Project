92e6238d15f5509b67b2f3ec18ec7a219a7821ab
Olena Serhata <olena.serhata@pitchbook.com>
2024-02-15 21:20:11 +0200

    Api tests

--- SerhataProj/src/main/java/api/dto/responseDto/PostsDto.java
+++ SerhataProj/src/main/java/api/dto/responseDto/PostsDto.java
@@ -1,16 +1,15 @@
 package api.dto.responseDto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PostsDto {
     @JsonProperty("_id")
    private String id;
@@ -23,14 +22,14 @@ public class PostsDto {
     private AuthorDTO author;
     private Boolean isVisitorOwner;
 
-    public PostsDto(String title, String body, String select, String uniquePost, AuthorDTO author, boolean isVisitorOwner) {
-        this.title = title;
-        this.body = body;
-        this.select = select;
-        this.uniquePost = uniquePost;
-        this.author = author;
-        this.isVisitorOwner = isVisitorOwner;
-    }
+   // public PostsDto(String title, String body, String select, String uniquePost, AuthorDTO author, boolean isVisitorOwner) {
+     //   this.title = title;
+      //  this.body = body;
+      //  this.select = select;
+      //  this.uniquePost = uniquePost;
+       // this.author = author;
+      //  this.isVisitorOwner = isVisitorOwner;
+    //}
 
 //    public String getId() {
 //        return id;
