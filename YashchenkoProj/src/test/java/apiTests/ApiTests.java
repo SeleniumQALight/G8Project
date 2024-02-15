69dff6518fa91cfaef3f2580fa784e1d3dc80bbb
tymofiiyashchenko <tymofii.yashchenko@airslate.com>
2024-02-15 19:32:45 +0200

    last lesson and refactor

--- YashchenkoProj/src/test/java/apiTests/ApiTests.java
+++ YashchenkoProj/src/test/java/apiTests/ApiTests.java
@@ -1,25 +1,69 @@
 package apiTests;
 
 import api.EndPoints;
+import api.dto.responseDto.AuthorDTO;
+import api.dto.responseDto.PostsDto;
 import io.restassured.http.ContentType;
 import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
 
 public class ApiTests {
     final String USER_NAME = "autoapi";
     Logger logger = Logger.getLogger(getClass());
 
     @Test
-    public void getAllPostsByUser(){
-        given()
+    public void getAllPostsByUser() {
+        PostsDto[] actualResponseAsDto =
+                given()
                 .contentType(ContentType.JSON)
                 .log().all()
                 .when()
                 .get(EndPoints.POSTS_BY_USER, USER_NAME)
                 .then().
                 log().all()
-                .statusCode(200);
+                .statusCode(SC_OK)
+                .assertThat()
+                .body("[0].title", equalTo("test2"))
+                .body("author.username", everyItem(equalTo(USER_NAME)))
+                .extract().body().as(PostsDto[].class);
+        logger.info(actualResponseAsDto[0].toString());
+        logger.info(actualResponseAsDto.length + " size");
+        logger.info("Title [0] = " + actualResponseAsDto[0].getTitle());
+        logger.info("UserName [0] = " + actualResponseAsDto[0].getAuthor().getUsername());
+
+        for (int i = 0; i < actualResponseAsDto.length; i++) {
+            Assert.assertEquals("Username is not matched in post " + i, USER_NAME, actualResponseAsDto[i].getAuthor().getUsername());
+        }
+
+        //Expected result
+        PostsDto[] expectedDto = {
+//                new PostsDto("test2", "test body2", "All Users", "no", new AuthorDTO(USER_NAME), false),
+//                new PostsDto("test", "test body", "All Users", "no", new AuthorDTO(USER_NAME), false)
+                PostsDto.builder()
+                        .title("test2").body("test body2").select("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username(USER_NAME).build()).isVisitorOwner(false)
+                        .build(),
+                PostsDto.builder()
+                        .title("test").body("test body").select("All Users").uniquePost("no")
+                        .author(AuthorDTO.builder().username(USER_NAME).build()).isVisitorOwner(false)
+                        .build()
+        };
+
+        Assert.assertEquals("Number of posts ", expectedDto.length, actualResponseAsDto.length);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(actualResponseAsDto)
+                        .usingRecursiveComparison()
+                .ignoringFields("id", "createdDate", "author.avatar")
+                                .isEqualTo(expectedDto);
+        softAssertions.assertAll();
+
     }
 }
