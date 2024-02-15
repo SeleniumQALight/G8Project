f5e5a55b04abd56dcd7d9e41f0fc57b43c701bc9
romio1371 <romio1371@gmail.com>
2024-02-15 21:51:15 +0200

    Create post part1

--- SvyrydiukProj/src/main/java/api/ApiHelper.java
+++ SvyrydiukProj/src/main/java/api/ApiHelper.java
@@ -1,14 +1,17 @@
 package api;
 
+import data.TestData;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.response.ResponseBody;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import static io.restassured.RestAssured.given;
 
@@ -36,6 +39,29 @@ public class ApiHelper {
 
     public ValidatableResponse getAllPostsByUserRequest(String userName) {
         return getAllPostsByUserRequest(userName, HttpStatus.SC_OK);
+    }
+
+    public String getToken() {
+        return getToken(TestData.VALID_LOGIN_API, TestData.VALID_PASSWORD_API);
+    }
+
+    public String getToken(String userName, String password) {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("username", userName);
+        requestBody.put("password", password);
+
+        ResponseBody responseBody =
+                given()
+                        .spec(requestSpecification)
+                        .body(requestBody.toMap())
+                        .when()
+                        .post(EndPoints.LOGIN)
+                        .then()
+                        .spec(responseSpecification)
+                        .extract()
+                        .response()
+                        .getBody();
+        return responseBody.asString().replace("\"", "");
     }
 
 }
--- SvyrydiukProj/src/main/java/data/TestData.java
+++ SvyrydiukProj/src/main/java/data/TestData.java
@@ -7,4 +7,8 @@ import libs.ConfigProvider;
 public class TestData {
     public final static String VALID_LOGIN_UI = System.getProperty("defaultLogin",ConfigProvider.configHiddenProperties.login());
     public final static String VALID_PASSWORD_UI = "123456qwerty";
+
+
+    public final static String VALID_LOGIN_API = "romanapi";
+    public final static String VALID_PASSWORD_API = "Roman123456!";
 }
