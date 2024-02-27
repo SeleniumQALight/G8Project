8f1dc85d0e105926b6d1355747efe2c5663b82b4
Vladyslava Sushko <vladyslava@mppteam.com>
2024-02-27 21:31:11 +0200

    added Valid Login cucumber test

---
+++ SushkoProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -0,0 +1,24 @@
+package bdd.stepDefinitions;
+
+import bdd.helpers.WebDriverHelper;
+import data.TestData;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPageSteps extends MainSteps{
+    public LoginPageSteps(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
+    @Given("I open LoginPage")
+    public void i_open_login_page() {
+        pageProvider.loginPage().openLoginPage();
+    }
+
+    @When("I login with valid cred")
+    public void iLoginWithValidCred() {
+        pageProvider.loginPage().enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
+        pageProvider.loginPage().enterTextIntoInputPassword(TestData.VALID_PASSWORD_UI);
+        pageProvider.loginPage().clickOnButtonSingIn();
+    }
+}
