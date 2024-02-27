f0fb3788e4c707fdb10b45a06d8f102a0d14f1e5
romio1371 <romio1371@gmail.com>
2024-02-27 21:43:16 +0200

    CW3_BDD test with cucumber_svyrydiuk-27-02

---
+++ SvyrydiukProj/src/main/java/bdd/stepDefinitions/HomePageSteps.java
@@ -0,0 +1,17 @@
+package bdd.stepDefinitions;
+
+import bdd.helpers.WebDriverHelper;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class HomePageSteps extends MainSteps{
+    public HomePageSteps(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
+    @Then("I see avatar on Home page")
+    public void iSeeAvatarOnHomePage() {
+        Assert.assertTrue(pageProvider.homePage().getHeader().isProfileButtonVisible());
+        Assert.assertTrue(pageProvider.homePage().getHeader().isButtonSignOutVisible());
+    }
+}
