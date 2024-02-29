09a1d0ad53c4ff615de7b29af1f022c68b906de5
HruhoriiMyhashko <ssadhu1992@gmail.com>
2024-02-29 12:18:54 +0200

    CW_5

---
+++ MyhashkoProj/src/main/java/bdd/stepDefinitions/HomePageSteps.java
@@ -0,0 +1,16 @@
+package bdd.stepDefinitions;
+
+import bdd.helpers.WebDriverHelper;
+import io.cucumber.java.en.Then;
+
+public class HomePageSteps extends MainSteps{
+    public HomePageSteps(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
+    @Then("I see avatar on Home page")
+    public void iSeeAvatarOnHomePage() {
+        pageProvider.homePage().getHeader().isButtonSignOutVisble();
+
+    }
+}
