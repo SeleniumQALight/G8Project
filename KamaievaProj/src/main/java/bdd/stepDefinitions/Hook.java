f04c67dce71619f91c75cd861dbc891742785908
KamaievaNataliia <nataliia.kamaeva@gmail.com>
2024-03-05 20:07:33 +0100

    before and after for case

--- KamaievaProj/src/main/java/bdd/stepDefinitions/Hook.java
+++ KamaievaProj/src/main/java/bdd/stepDefinitions/Hook.java
@@ -1,5 +1,6 @@
 package bdd.stepDefinitions;
 
+import api.ApiHelper;
 import bdd.helpers.WebDriverHelper;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -7,17 +8,24 @@ import io.cucumber.java.Before;
 public class Hook {
 
     WebDriverHelper webDriverHelper;
+    ApiHelper apiHelper = new ApiHelper();
     public Hook(WebDriverHelper webdriverHelper){
         this.webDriverHelper = webdriverHelper;
     }
-    @Before
+    @Before(order = 0)
     public void setUp() {
 
     }
 
-    @After
+    @After(order = 0)
     public void tearDown() {
         webDriverHelper.quitDriver();
+    }
+
+    @Before(value = "@deleteAllPostsForDefaultUser", order = 50)
+    @After(value = "@deleteAllPostsForDefaultUser", order = 50)
+    public void deletePostsForDefaultUser() {
+        apiHelper.deleteAllPostsTillPresent();
     }
 
 }
