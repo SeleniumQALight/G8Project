d24e3d726dbcdef8948f8f6ff4a1a1f42eb09d19
Anton Kraynov <qa.anton.k@alldigitalads.com>
2024-03-05 21:06:30 +0200

    add new method for delete all posts of default user

--- KraynovProj/src/main/java/bdd/stepDefinitions/Hook.java
+++ KraynovProj/src/main/java/bdd/stepDefinitions/Hook.java
@@ -1,24 +1,32 @@
 package bdd.stepDefinitions;
 
+import api.ApiHelper;
 import bdd.helpers.WebDriverHelper;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hook {
     WebDriverHelper webDriverHelper;
+    ApiHelper apiHelper = new ApiHelper();
 
     public Hook(WebDriverHelper webDriverHelper) {
         this.webDriverHelper = webDriverHelper;
     }
 
-    @Before
+    @Before(order = 0)
     public void setUp() {
 
     }
 
-    @After
+    @After(order = 100)
     public void tearDown(){
         webDriverHelper.quitDriver();
+    }
+
+    @Before(value = "@deleteAllPostsForDefaultUser", order = 50)
+    @After(value = "@deleteAllPostsForDefaultUser", order = 50)
+    public void deletePostsForDefaultUser(){
+        apiHelper.deleteAllPostsTillPresent();
     }
 }
 
