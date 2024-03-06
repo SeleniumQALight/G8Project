1638141ff3141f05e88d0cb8f454747bafa93c2d
Anton Kraynov <qa.anton.k@alldigitalads.com>
2024-03-06 22:54:50 +0200

    add new scenario bdd with checking error messages

--- KraynovProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
+++ KraynovProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -10,6 +10,7 @@ import io.cucumber.java.en.When;
 import pages.PageProvider;
 
 public class LoginPageSteps extends MainSteps {
+    final String LONG_PASSWORD = "tr_x30";
     public LoginPageSteps(WebDriverHelper webDriverHelper) {
         super(webDriverHelper);
     }
@@ -46,6 +47,28 @@ public class LoginPageSteps extends MainSteps {
     @Then("I see alert message with text {string}")
     public void iSeeAlertMessageWithTextInvalidUsernamePassword(String message) {
         pageProvider.loginPage().checkTextInAlertInCenter(message);
+    }
 
+    @When("I enter {string} into input Username in Registration form in Login page")
+    public void iEnterIntoInputUsernameInRegistrationFormInLoginPage(String userName) {
+        pageProvider.loginPage().enterTextIntoInputUsernameRegistration(userName);
+    }
+
+    @When("I enter {string} into input Email in Registration form in Login page")
+    public void iEnterIntoInputEmailInRegistrationFormInLoginPage(String email) {
+        pageProvider.loginPage().enterTextIntoInputEmailRegistration(email);
+    }
+
+    @When("I enter {string} into input Password in Registration form in Login page")
+    public void iEnterIntoInputPasswordInRegistrationFormInLoginPage(String password) {
+        if (password.equalsIgnoreCase(LONG_PASSWORD)) {
+            password = LONG_PASSWORD.repeat(30);
+        }
+        pageProvider.loginPage().enterTextIntoInputPasswordRegistration(password);
+    }
+
+    @Then("I see warning messages {string} in the  fields.")
+    public void iSeeWarningMessagesInTheFields(String expectedMessage) {
+        pageProvider.loginPage().checkErrorsMessages(expectedMessage);
     }
 }
