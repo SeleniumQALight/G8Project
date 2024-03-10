b21c4655c91024d8ceb9390e33e4e9ec2d4d19c0
HruhoriiMyhashko <ssadhu1992@gmail.com>
2024-03-10 03:58:35 +0200

    Exam_4_Myhashko

--- MyhashkoProj/src/main/java/api/EndPointsPrivat.java
+++ MyhashkoProj/src/main/java/api/EndPointsPrivat.java
@@ -1,6 +1,10 @@
 package api;
 
+import java.net.URI;
+
 public interface EndPointsPrivat {
     String BASE_URL = "https://api.privatbank.ua";
     String EXCHANGE_RATES = BASE_URL + "/p24api/exchange_rates";
+    String PRIVATE_EXAM = BASE_URL + "/p24api/pubinfo?json&exchange&coursid=5";
+    String PRIVATE_START_PAGE_EXAM = "https://privatbank.ua/";
 }
---
+++ MyhashkoProj/src/main/java/pages/PrivatBankStartPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import api.EndPointsPrivat;
+import data.TestData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PrivatBankStartPage extends ParentPage{
+    public PrivatBankStartPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @Override
+    protected String getRelativeUrl() {
+            return "/";
+    }
+
+    public PrivatBankStartPage openPrivatBankStartPage() {
+        webDriver.get(EndPointsPrivat.PRIVATE_START_PAGE_EXAM);
+        webDriver.manage().window().maximize();
+
+        return this;
+    }
+
+    public void getCurrencyRateWithUiInPrivatBank(String currency) {
+        String locator_buy = String.format(".//*[@id='%s_buy']", currency);
+        String locator_sale = String.format(".//*[@id='%s_sell']", currency);
+
+        WebElement buy = webDriver.findElement(By.xpath(locator_buy));
+        WebElement sale = webDriver.findElement(By.xpath(locator_sale));
+
+        TestData.ui_curs_buy = buy.getText();
+        logger.info("ui curs buy for " + currency + " = " + TestData.ui_curs_buy);
+        TestData.ui_curs_sale = sale.getText();
+        logger.info("ui curs sale for " + currency + " = " + TestData.ui_curs_sale);
+    }
+}
