7a1fbf4a40834ea18369a05856a09158c02415b0
HruhoriiMyhashko <ssadhu1992@gmail.com>
2024-03-10 15:31:16 +0200

    PRIVATE_EXAM = BASE_URL + "/p24api/pubinfo?json&exchange&coursid=5"; замінив на PRIVATE_EXAM = BASE_URL + "/p24api/pubinfo";

--- MyhashkoProj/src/main/java/api/EndPointsPrivat.java
+++ MyhashkoProj/src/main/java/api/EndPointsPrivat.java
@@ -5,6 +5,6 @@ import java.net.URI;
 public interface EndPointsPrivat {
     String BASE_URL = "https://api.privatbank.ua";
     String EXCHANGE_RATES = BASE_URL + "/p24api/exchange_rates";
-    String PRIVATE_EXAM = BASE_URL + "/p24api/pubinfo?json&exchange&coursid=5";
+    String PRIVATE_EXAM = BASE_URL + "/p24api/pubinfo";
     String PRIVATE_START_PAGE_EXAM = "https://privatbank.ua/";
 }
