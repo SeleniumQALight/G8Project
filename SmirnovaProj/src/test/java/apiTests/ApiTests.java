b39dacb7d56f26716cd0e6c82beacea02cc5cc74
a.smirnova <nastyanastina9@gmail.com>
2024-03-10 15:37:49 +0100

    add Api tests

--- SmirnovaProj/src/test/java/apiTests/ApiTests.java
+++ SmirnovaProj/src/test/java/apiTests/ApiTests.java
@@ -1,22 +1,30 @@
 package apiTests;
 
+import api.ApiHelper;
 import api.Endpoints;
 import api.dto.response.AuthorDto;
 import api.dto.response.PostsDto;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
 
 public class ApiTests {
     final String USER_NAME = "autoapi";
     Logger logger = Logger.getLogger(getClass());
+    ApiHelper apiHelper = new ApiHelper();
 
     @Test
     public void getAllPostsByUser() {
@@ -45,8 +53,24 @@ public class ApiTests {
 
         //Expected result:
         PostsDto[] expectedDto = {
-                new PostsDto("test2", "test body2", "All Users", "no", new AuthorDto(USER_NAME), false),
-                new PostsDto("test", "test body", "All Users", "no", new AuthorDto(USER_NAME), false),
+//                new PostsDto("test2", "test body2", "All Users", "no", new AuthorDto(USER_NAME), false),
+//                new PostsDto("test", "test body", "All Users", "no", new AuthorDto(USER_NAME), false),
+                PostsDto.builder()
+                        .title("test2")
+                        .body("test body2")
+                        .select("All Users")
+                        .uniquePost("no")
+                        .author(AuthorDto.builder().username(USER_NAME).build())
+                        .isVisitorOwner(false)
+                        .build(),
+                PostsDto.builder()
+                        .title("test")
+                        .body("test body")
+                        .select("All Users")
+                        .uniquePost("no")
+                        .author(AuthorDto.builder().username(USER_NAME).build())
+                        .isVisitorOwner(false)
+                        .build()
         };
 
         Assert.assertEquals("Number of posts ", expectedDto.length, actualResponseAsDto.length);
@@ -60,5 +84,42 @@ public class ApiTests {
         softAssertions.assertAll();
 
 
+    }
+
+    @Test
+    public void getAllPostsByUserNegative() {
+        final String INVALID_USER_NAME = "invalidUsername";
+        String actualResponse =
+                apiHelper.getAllPostsByUserRequest(INVALID_USER_NAME, 400)
+                        .extract().response().body().asString();
+        Assert.assertEquals("Message in response",
+                "\"Sorry, invalid user requested. Wrong username - invalidUsername or there is no posts. Exception is undefined\"",
+                actualResponse);
+    }
+
+    @Test
+    public void getAllPostsByUserPath() {
+        Response actualResponse = apiHelper.getAllPostsByUserRequest(USER_NAME).extract().response();
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        List<String> actualTitles = actualResponse.jsonPath().getList("title", String.class);
+        for (int i = 0; i < actualTitles.size(); i++) {
+            softAssertions.assertThat(actualTitles.get(i)).as("Item number " + i)
+                    .contains("test");
+        }
+
+        List<Map> actualAuthorsList = actualResponse.jsonPath().getList("author", Map.class);
+        for (int i = 0; i < actualAuthorsList.size(); i++) {
+            softAssertions.assertThat(actualAuthorsList.get(i).get("username")).as("Item number " + i)
+                    .isEqualTo(USER_NAME);
+        }
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    public void getAllPostsByUserSchema() {
+        apiHelper.getAllPostsByUserRequest(USER_NAME)
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
     }
 }
