3a02a5512d9db80ffbcbcd93409a3e262db4bca8
tanyaBulakh <bulakhtanya94@gmail.com>
2024-03-12 13:57:04 +0200

    Exam project bdd

--- BulakhProj/src/main/java/apiPrivatBank/EndPointsPrivat.java
+++ BulakhProj/src/main/java/apiPrivatBank/EndPointsPrivat.java
@@ -3,6 +3,8 @@ package apiPrivatBank;
 public interface EndPointsPrivat {
 
     String BASE_URL = "https://api.privatbank.ua/p24api/exchange_rates";
+    String EXCHANGE_RATE = "https://api.privatbank.ua/p24api/pubinfo";
+
 
 
 }
