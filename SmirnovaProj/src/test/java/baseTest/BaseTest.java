0b453d1fc6eb0192777c02f35a489e3fdb0d01cf
a.smirnova <nastyanastina9@gmail.com>
2024-03-14 19:12:31 +0100

    add settings for grid

--- SmirnovaProj/src/test/java/baseTest/BaseTest.java
+++ SmirnovaProj/src/test/java/baseTest/BaseTest.java
@@ -8,20 +8,26 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import pages.PageProvider;
 import pages.ParentPage;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class BaseTest {
     WebDriver webDriver;
     protected Logger logger = Logger.getLogger(getClass());
     protected PageProvider pageProvider;
-     ParentPage parentPage;
+    ParentPage parentPage;
+
     //Буде виконуватися перед кожним тестом
     @Before
     public void setUp() {
@@ -44,15 +50,16 @@ public class BaseTest {
 
     @Rule
     public TestName testName = new TestName();
-    private WebDriver initDriver(){
+
+    private WebDriver initDriver() {
         String browser = System.getProperty("browser");
-        if ((browser == null) || ("chrome".equals(browser.toLowerCase()))){ // default browser -Dbrowser=chrome
+        if ((browser == null) || ("chrome".equals(browser.toLowerCase()))) { // default browser -Dbrowser=chrome
             WebDriverManager.chromedriver().setup();
             webDriver = new ChromeDriver();
-        } else if ("firefox".equals(browser.toLowerCase())){ // -Dbrowser=firefox
+        } else if ("firefox".equals(browser.toLowerCase())) { // -Dbrowser=firefox
             WebDriverManager.firefoxdriver().setup();
             webDriver = new FirefoxDriver();
-        } else if ("ie".equals(browser.toLowerCase())){
+        } else if ("ie".equals(browser.toLowerCase())) {
             WebDriverManager.iedriver().setup(); //zoom 100%
             webDriver = new InternetExplorerDriver(); //security level - Medium
         } else if ("safari".equalsIgnoreCase(browser)) {
@@ -61,7 +68,21 @@ public class BaseTest {
         } else if ("edge".equalsIgnoreCase(browser)) {
             WebDriverManager.edgedriver().setup();
             webDriver = new EdgeDriver();
-        }  else {
+        } else if ("remote".equals(browser)) {
+            logger.info("Remote browser");
+            WebDriverManager.chromedriver().setup();
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setBrowserName("chrome");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.merge(cap);
+            try {
+                webDriver = new RemoteWebDriver(
+                        new URL("http://localhost:4444/wd/hub"),
+                        chromeOptions);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else {
             throw new IllegalArgumentException("Browser " + browser + " is not supported");
         }
         return webDriver;
