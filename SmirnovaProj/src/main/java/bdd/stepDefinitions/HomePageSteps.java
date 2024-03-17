08ed2731e3a20dc14933439644502e217a3a7109
a.smirnova <nastyanastina9@gmail.com>
2024-03-17 10:50:31 +0100

    test CreatePost via cucumber

--- SmirnovaProj/src/main/java/bdd/stepDefinitions/HomePageSteps.java
+++ SmirnovaProj/src/main/java/bdd/stepDefinitions/HomePageSteps.java
@@ -1,9 +1,14 @@
 package bdd.stepDefinitions;
 
 import bdd.helpers.WebDriverHelper;
+import data.TestData;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class HomePageSteps extends MainSteps {
+    final String DEFAULT = "default";
+
     public HomePageSteps(WebDriverHelper webDriverHelper) {
         super(webDriverHelper);
     }
@@ -11,5 +16,23 @@ public class HomePageSteps extends MainSteps {
     @Then("I see avatar on Home page")
     public void iSeeAvatarOnHomePage() {
         pageProvider.homePage().getHeader().checkIsAvatarDisplayed();
+    }
+
+    @Given("I open Home page as {string} user and {string} password")
+    public void iOpenHomePageAsDefaultUserAndDefaultPassword(String userName, String password) {
+        if (DEFAULT.equalsIgnoreCase(userName)) {
+            userName = TestData.VALID_LOGIN_API;
+        }
+        if (DEFAULT.equalsIgnoreCase(password)) {
+            password = TestData.VALID_PASSWORD_API;
+        }
+        pageProvider.loginPage().fillLoginFormAndSubmit(userName, password);
+        pageProvider.homePage().checkIsRedirectToHomePage();
+
+    }
+
+    @When("I click on MyProfile button")
+    public void iClickOnMyProfileButton() {
+        pageProvider.homePage().getHeader().clickOnMyProfileButton();
     }
 }
--- SmirnovaProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
+++ SmirnovaProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -2,6 +2,7 @@ package bdd.stepDefinitions;
 
 import bdd.helpers.WebDriverHelper;
 import data.TestData;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -23,4 +24,23 @@ public class LoginPageSteps extends MainSteps {
         pageProvider.loginPage().clickOnButtonSignIn();
     }
 
+    @When("I enter {string} into input Login in Login page")
+    public void iEnterLoginIntoInputLoginInLoginPage(String login) {
+        pageProvider.loginPage().enterTextIntoInputLogin(login);
+    }
+
+    @And("I enter {string} into input Password in Login page")
+    public void iEnterPasswordIntoInputPasswordInLoginPage(String password) {
+        pageProvider.loginPage().enterTextIntoInputPassword(password);
+    }
+
+    @And("I click on button Login in Login page")
+    public void iClickOnButtonLoginInLoginPage() {
+        pageProvider.loginPage().clickOnButtonSignIn();
+    }
+
+    @Then("I see error message with text {string}")
+    public void iSeeErrorMessageWithTextInvalidUsernamePassword(String errorMessage) {
+        pageProvider.loginPage().checkErrorsMessagesLogin(errorMessage);
+    }
 }
