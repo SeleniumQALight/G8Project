47ece3a6722187d0e53058d0637482551d62468d
Nikita <qa.nikita.p@alldigitalads.com>
2024-03-17 18:24:51 +0200

    Create Exam Project

--- PavlykProj/src/main/java/apiPrivatBank/EndPointsPrivatBank.java
+++ PavlykProj/src/main/java/apiPrivatBank/EndPointsPrivatBank.java
@@ -3,4 +3,5 @@ package apiPrivatBank;
 public interface EndPointsPrivatBank {
 
     String BASE_URL = "https://api.privatbank.ua/p24api/exchange_rates";
+    String EXCHANGE = "https://api.privatbank.ua/p24api/pubinfo";
 }
---
+++ PavlykProj/src/main/java/apiPrivatBank/dto/responsePrivatBankDTO/CurrencyRateDto.java
@@ -0,0 +1,16 @@
+package apiPrivatBank.dto.responsePrivatBankDTO;
+
+import lombok.*;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CurrencyRateDto {
+
+    private String ccy;
+    private String base_ccy;
+    private String buy;
+    private String sale;
+}
